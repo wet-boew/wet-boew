d56d6ebcd2b26be8a36173114eaa8a5a1888b3d2
Paul Jackson <Paul.Jackson@tbs-sct.gc.ca>
2012-05-23 15:52:52 -0400

    Changed Search term highlighting to Text highlighting. Made it work through the query string only. Added support for multi-word strings. Added a French working example.

---
+++ src/js/workers/texthighlight.js
@@ -0,0 +1,78 @@
+/*!
+ * Web Experience Toolkit (WET) / Boîte à outils de l'expérience Web (BOEW)
+ * www.tbs.gc.ca/ws-nw/wet-boew/terms / www.sct.gc.ca/ws-nw/wet-boew/conditions
+ */
+/*
+ * Text highlighting functionality 
+ */
+/*global jQuery: false, pe: false, dictionary: false*/
+(function ($) {
+	var _pe = window.pe || {
+		fn: {}
+	}; /* local reference */
+	_pe.fn.texthighlight = {
+		type: 'plugin',
+		_exec: function (elm) {
+			var searchCriteria, queryParams = pe.url(document.location).params || "";
+			if (queryParams['texthighlight'] != null) {
+				addHighlight(queryParams['texthighlight'], elm);
+			}
+/*
+* addHighlight
+*
+* This function highlights any text within a pre-defined area that matches search criteria specified through the URL's query string.
+*
+* @param text string The text that we want to search for.
+* @param target Object The DOM object to look into
+* @param settings Object The plugins settings
+*
+* @return (nothing) Just updates the content directly
+*
+*/
+
+			function addHighlight(searchCriteria, target) {
+				var arrSearchCriteria, newText, i, matches = 0;
+				searchCriteria = searchCriteria.replace(/^\s+|\s+$/g, '');
+				searchCriteria = searchCriteria.replace(/\|+/g, ''); // don't let them use the | symbol
+				// --------------------------------------------------------------------------------------------
+				// Split the data into an array so that we can exclude anything smaller than the minimum length
+				arrSearchCriteria = searchCriteria.split('+');
+				if (arrSearchCriteria.length > 0) {
+					searchCriteria = '';
+					for (i = 0; i < arrSearchCriteria.length; i += 1) {
+						searchCriteria += arrSearchCriteria[i] + " ";
+					}
+					searchCriteria = searchCriteria.replace(/^\s+|\s+$|\"|\(|\)/g, '');
+				}
+				searchCriteria = searchCriteria.replace(/\s+/g, '|'); // OR each value
+				searchCriteria = decodeURIComponent(searchCriteria);
+				searchCriteria = "(?=([^>]*<))([\\s'])?(" + searchCriteria + ")(?!>)"; // Make sure that we're not checking for text within a tag; only the text outside of tags.
+				// --------------------------------------------------------------------------------------------
+				newText = target.html().replace(new RegExp(searchCriteria, "gi"), function (match, grp1, grp2, grp3) {
+					return grp2 + '<span class="texthighlight"><mark>' + grp3 + '</mark></span>';
+				});
+				target.html(newText);
+				return null;
+			} // end of addHighlight
+/*
+ * clearHighlight
+ *
+ * This function will clear out any text that is not supposed to be highlighted anymore
+ *
+ * @param target Object The DOM object to clear
+ * @return (nothing) Just updates the content directly
+ *
+ */
+
+		function clearHighlight(target) {
+				target.find('span.texthighlight').each(function () {
+					var text = $(this).text();
+					$(this).replaceWith(text);
+				});
+			} // end of clearHighlight
+			return this;
+		} // end of exec
+	};
+	window.pe = _pe;
+	return _pe;
+}(jQuery));
