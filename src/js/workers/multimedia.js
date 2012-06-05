2e56007a9fe212d1e4c4b82b6d11916ce0791bff
Laurent Goderre <>
2012-06-05 10:28:27 -0400

    Added the Flash fallback logic

--- src/js/workers/multimedia.js
+++ src/js/workers/multimedia.js
@@ -15,10 +15,11 @@
 		type: 'plugin',
                 
 		_exec: function (elm) {
-			var opts,
-				aVariable,
-				anotherVariable;
-			opts = {
+			var id,
+			    canPlay = false,
+			    media = elm.children("audio, video").eq(0);
+
+			/*opts = {
 				// This is an example from tabbedinterface, to show how to pass configuration parameters from the html element to the plugin.
 				// There are some simple examples here, along with some more complicated ones.
 				defaultElm: ((elm.find(".default").length) ? ".default" : "li:first-child"),
@@ -28,7 +29,58 @@
 				autoPlay: (elm.hasClass("auto-play") ? true : false),
 				animate: (elm.hasClass("animate") || elm.hasClass("animate-slow") || elm.hasClass("animate-fast") ? true : false),
 				animationSpeed: (elm.hasClass("animate-slow") ? "slow" : (elm.hasClass("animate-fast") ? "fast" : "normal"))
-			};
+			};*/
+
+			var id;
+			//Add an id if an id is missing
+			if (elm.attr("id") !== undefined){
+			    id = elm.attr("id");
+			}else{
+			    id = "wet-boew-mediaplayer" + elm.index();
+			    elm.attr("id", id);
+			}
+
+			if (media.get(0).currentSrc !== "" && media.get(0).currentSrc !== undefined){
+			    canPlay = true;
+			}else{
+			    //No nativly supported format provided, trying Flash fallback
+			    //TODO:Add Flash detection
+			    flash = true;
+
+			    var fbVideoType = "video/mp4";
+			    var fbAudioType = "audio/mpeg"; //MP3
+
+			    var canFB = false;
+			    var $fbObject = $("<object type=\"application/x-shockwave-flash\" data=\"" + _pe.add.liblocation + "/bin/multimedia.swf\" tabindex=\"-1\"></object>");
+			    var $fbVars = $("<param name=\"flashvars\" value=\"id=" + elm.attr("id") +"\"/>");
+			    $fbObject.append($fbVars);
+			    if (flash && media.is("video") && media.find("source[type=\"" + fbVideoType + "\"]")){
+				$fbObject.addClass("video");
+				$fbObject.attr("width", media.width()).attr("height", media.height());
+				$fbObject.append("<param value=\"always\" name=\"allowScriptAccess\"><param value=\"#000000\" name=\"bgcolor\"><param value=\"opaque\" name=\"wmode\">");
+				$fbVars.attr("value", $fbVars.attr("value") + "&height=" + media.height() + "&width=" + media.width() + "&posterimg=" + escape(_pe.url(media.attr("poster")).relative) + "&media=" + _pe.url(media.find("source[type=\"" + fbVideoType + "\"]").attr("src")).relative);
+				canFB = true;
+			    }else if (flash && media.is("audio") && media.find("source[type=\"" + fbAudioType + "\"]")){
+				$fbObject.addClass("audio");
+				$fbVars.attr("value", $fbVars.attr("value") + "&media=" + _pe.url(media.find("source[type=\"" + fbAudioType + "\"]").attr("src")).relative);
+				$fbObject.append($fbVars);
+				canFB = true;
+			    }else{
+				//TODO: Display when no HTML5 video or Flash
+				canPlay = false;
+			    }
+
+			    if (canFB){
+				media.before($fbObject);
+				media.remove();
+				media = $fbObject;
+			    }
+			}
+
+			if (canPlay){
+
+			}
+
 			// Do plugin stuff here...
 			return elm;
 		}, // end of exec
@@ -107,6 +159,12 @@
                     }
                 }
 	};
+
+	//Method to allow the flash player to trigger the media events
+	_pe.triggermediaevent = function(id, event, test){
+	    var o = $("#" + id).find("object :first-child").trigger(event);
+	}
+
 	window.pe = _pe;
 	return _pe;
 }(jQuery));
