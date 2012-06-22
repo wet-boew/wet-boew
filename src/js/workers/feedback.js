b2386e61e0f94a267782d8fa466ee9921e339c54
Paul Jackson <Paul.Jackson@tbs-sct.gc.ca>
2012-06-22 01:58:58 -0400

    Fixed numerous glitches with Feedback form and Form validation.

--- src/js/workers/feedback.js
+++ src/js/workers/feedback.js
@@ -15,59 +15,73 @@
 		type: 'plugin',
 		depends: [],
 		_exec: function (elm) {
-			var feedback = elm.find('#feedback').attr('aria-controls', 'web'),
+			var feedback = elm.find('#feedback'),
 				web = elm.find('#web'),
-				access = web.find('#access').attr('aria-controls', 'mobile computer'),
+				access = web.find('#access'),
 				mobile = web.find('#mobile'),
 				computer = web.find('#computer'),
-				contact_coord = elm.find('contact-coord'),
+				contact_coord = elm.find('#contact-coord'),
 				contact1 = contact_coord.find('#contact1'),
 				contact2 = contact_coord.find('#contact2'),
 				info = contact_coord.find('#info'),
-				referrerUrl = document.referrer;
+				referrerUrl = document.referrer,
+				load;
 
 			// Web Questions
-			feedback.on("keyup click load", function () {
-				if ($(this).val() === 'web') {
+			feedback.attr('aria-controls', 'web').on("keyup click load", function (e) {
+				load = (e.type === "load");
+				if (!load && $(this).val() === 'web') {
 					web.attr('aria-hidden', 'false').show("slow");
 				} else {
-					web.attr('aria-hidden', 'true').hide("slow");
+					web.attr('aria-hidden', 'true').hide((load ? "" : "slow"));
 				}
 			});
 			// Automatically select the reason if specified in the query string
-			feedback.find('option[value="' + pe.url(document.location).params.feedback + '"]').attr("selected", "selected").trigger("load");
+			feedback.find('option[value="' + pe.url(document.location).params.feedback + '"]').attr("selected", "selected");
+			feedback.trigger("load");
 
 			// Computer and Mobile
-			access.on("keyup click load", function () {
-				if (access.val() === 'mobile') {
+			access.attr('aria-controls', 'mobile computer').on("keyup click load", function (e) {
+				load = (e.type === "load");
+				if (!load && access.val() === 'mobile') {
 					mobile.attr('aria-hidden', 'false').show("slow");
 					computer.attr('aria-hidden', 'true').hide("slow");
 				} else {
-					computer.attr('aria-hidden', 'false').show("slow");
-					mobile.attr('aria-hidden', 'true').hide("slow");
+					computer.attr('aria-hidden', 'false').show((load ? "" : "slow"));
+					mobile.attr('aria-hidden', 'true').hide((load ? "" : "slow"));
 				}
 			}).trigger("load");
 
 			// Contact info first selection
-			contact1.on("keyup click load", function () {
-				if ($(this).val() === 'yes') {
+			contact1.on("keyup click load", function (e) {
+				load = (e.type === "load");
+				if (!load && $(this).val() === 'yes') {
 					info.attr('aria-hidden', 'false').show("slow");
-				} else if ($(this).val() === 'no' && (contact2.val() === 'no' || contact2.val() === null)) {
-					info.attr('aria-hidden', 'true').hide("slow");
+				} else if (load || (($(this).val() === 'no' || $(this).val() === null) && (contact2.val() === 'no' || contact2.val() === null))) {
+					info.attr('aria-hidden', 'true').hide((load ? "" : "slow"));
 				}
-			}).trigger("load");
+			}).trigger("click");
 
 			// Contact info second selection
-			contact2.on("keyup click load", function () {
-				if ($(this).val() === 'yes') {
+			contact2.on("keyup click load", function (e) {
+				load = (e.type === "load");
+				if (!load && $(this).val() === 'yes') {
 					info.attr('aria-hidden', 'false').show("slow");
-				} else if ($(this).val() === 'no' && (contact1.val() === 'no' || contact1.val() === null)) {
-					info.attr('aria-hidden', 'true').hide("slow");
+				} else if (load || (($(this).val() === 'no' || $(this).val() === null) && (contact1.val() === 'no' || contact1.val() === null))) {
+					info.attr('aria-hidden', 'true').hide((load ? "" : "slow"));
 				}
 			}).trigger("load");
 
 			// Prepopulates URL form field with referrer
 			web.find('#page').attr('value', referrerUrl);
+
+			// Return to the form defaults when the reset button is activated
+			elm.find('input[type=reset]').on('click', function () {
+				feedback.trigger("load");
+				access.trigger("load");
+				contact1.trigger("load");
+				contact2.trigger("load");
+			});
 
 			return elm;
 		} // end of exec
