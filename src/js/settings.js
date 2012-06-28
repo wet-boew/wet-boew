93bfc0a1a14885f2e30d9e994e55b892e76c93ec
Paul Jackson <Paul.Jackson@tbs-sct.gc.ca>
2012-06-28 16:00:30 -0400

    Added deselectable radio buttons support and enabled it globally through setting.js.

--- src/js/settings.js
+++ src/js/settings.js
@@ -7,7 +7,7 @@ WET-BOEW-Settings
  */
 var wet_boew_properties = {
 	/** global plugins are called via a array of dependency names **/
-	globals : ['equalize', 'css3ie', /*, 'toolbar'*/]
+	globals : ['equalize', 'deselectradio', 'css3ie'/*, 'toolbar'*/]
 };
 
 /*
