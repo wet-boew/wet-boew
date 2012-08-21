8fdd65a072a65dfa9ab1998554edb9be77ffedf7
pdubois <pierre.dubois@ic.gc.ca>
2012-08-21 16:28:35 -0400

    fixed a minor issue with the table parser caused by the previous commit

--- src/js/workers/parser.table.js
+++ src/js/workers/parser.table.js
@@ -560,7 +560,21 @@
 							}
 
 							// Skip this colgroup, this should happened only once and should represent the header colgroup
-							// return;
+							
+							// TODO: Assign the headers for this group
+							for (i = 0, _ilen = curColgroupFrame.col.length; i < _ilen; i += 1) {
+								gzCol = curColgroupFrame.col[i];
+								gzCol.header = [];
+								for (j = 0, _jlen = tmpStack.length; j < _jlen; j += 1) {
+									for (m = gzCol.start, _mlen = gzCol.end; m <= _mlen; m += 1) {
+										if (j == 0 || (j > 1 && tmpStack[j].cell[m - 1].uid !== tmpStack[j - 1].cell[m - 1].uid)) {
+											gzCol.header.push(tmpStack[j].cell[m - 1]);
+										}
+									}
+								}
+							}
+							
+							return;
 						}
 
 						groupLevel = undefined;
