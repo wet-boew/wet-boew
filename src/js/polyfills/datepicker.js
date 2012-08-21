92b633ed49158276dbf9395e674e53b9d84b31a4
Paul Jackson <Paul.Jackson@tbs-sct.gc.ca>
2012-08-20 20:50:05 -0400

    Merged in #241. Fixed datepicker button in mobile view.

--- src/js/polyfills/datepicker.js
+++ src/js/polyfills/datepicker.js
@@ -18,7 +18,7 @@
 				createToggleIcon,
 				date = new Date(),
 				field,
-				format = "YYYY-MM-DD",
+				format = 'YYYY-MM-DD',
 				formatDate,
 				hide,
 				hideAll,
@@ -34,10 +34,10 @@
 				wrapper,
 				elm = $(this);
 
-			elm.addClass("picker-field");
+			elm.addClass('picker-field');
 
 			createToggleIcon = function (fieldid, containerid) {
-				var fieldLabel = wrapper.find("label[for='" + fieldid + "']").text(),
+				var fieldLabel = wrapper.find('label[for="' + fieldid + '"]').text(),
 					objToggle = $('<a id="' + containerid + '-toggle" class="picker-toggle-hidden" href="javascript:;"><img src="' + pe.add.liblocation + 'images/datepicker/icon.png" alt="' + pe.dic.get('%datepicker-show') + fieldLabel + '"/></a>');
 
 				objToggle.on('click', function () {
@@ -58,8 +58,8 @@
 
 				days.each(function (index, value) {
 					if ((!lLimit && !hLimit) || (lLimit === true && index >= minDate.getDate()) || (hLimit === true && index <= maxDate.getDate())) {
-						var obj = $(value).children("div"),
-							link = $("<a href=\"javascript:;\"></a>"),
+						var obj = $(value).children('div'),
+							link = $('<a href="javascript:;"></a>'),
 							parent = obj.parent();
 						parent.empty();
 						link.append(obj.html());
@@ -72,7 +72,7 @@
 							if (!(event.ctrlKey || event.altKey || event.metaKey)) {
 								switch (event.keyCode) {
 								case 27: // escape key
-									toggle(fieldid, fieldid + "-picker");
+									toggle(fieldid, fieldid + '-picker');
 									return false;
 								case 32: // spacebar
 									$(this).trigger('click');
@@ -128,7 +128,7 @@
 							addSelectedDateToField(event.data.fieldid, event.data.year, event.data.month + 1, event.data.day, event.data.format);
 							setSelectedDate(event.data.fieldid, event.data.year, event.data.month, event.data.days, event.data.format);
 							//Hide the calendar on selection
-							toggle(event.data.fieldid, event.data.fieldid + "-picker");
+							toggle(event.data.fieldid, event.data.fieldid + '-picker');
 							return false;
 						});
 					}
@@ -154,28 +154,28 @@
 			setSelectedDate = function (fieldid, year, month, days, format) {
 				var pattern, date, cpntDate, regex;
 				//Reset selection state
-				$(days).removeClass("datepicker-selected");
-				$(days).find(".datepicker-selected-text").detach();
+				$(days).removeClass('datepicker-selected');
+				$(days).find('.datepicker-selected-text').detach();
 
 				//Create regular expression to match value (Note: Using a, b and c to avoid replacing conflicts)
-				format = format.replace("DD", "(?<a> [0-9]{2})");
-				format = format.replace("D", "(?<a> [0-9] )");
-				format = format.replace("MM", "(?<b> [0-9]{2})");
-				format = format.replace("M", "(?<b> [0-9])");
-				format = format.replace("YYYY", "(?<c> [0-9]{4})");
-				format = format.replace("YY", "(?<c /> [0-9]{2})");
-				pattern = "^" + format + "$";
+				format = format.replace('DD', '(?<a> [0-9]{2})');
+				format = format.replace('D', '(?<a> [0-9] )');
+				format = format.replace('MM', '(?<b> [0-9]{2})');
+				format = format.replace('M', '(?<b> [0-9])');
+				format = format.replace('YYYY', '(?<c> [0-9]{4})');
+				format = format.replace('YY', '(?<c /> [0-9]{2})');
+				pattern = '^' + format + '$';
 
 				//Get the date from the field
-				date = $("#" + fieldid).attr("value");
-				regex = new XRegExp(pattern, "x");
+				date = $('#' + fieldid).attr('value');
+				regex = new XRegExp(pattern, 'x');
 
 				try {
 					if (date !== '') {
 						cpntDate = $.parseJSON(date.replace(regex, '{"year":"${c}", "month":"${b}", "day":"${a}"}'));
 						if (cpntDate.year === year && cpntDate.month === month + 1) {
-							$(days[cpntDate.day - 1]).addClass("datepicker-selected");
-							$(days[cpntDate.day - 1]).children("a").append('<span class="wb-invisible datepicker-selected-text"> [' + pe.dic.get("%datepicker-selected") + ']</span>');
+							$(days[cpntDate.day - 1]).addClass('datepicker-selected');
+							$(days[cpntDate.day - 1]).children('a').append('<span class="wb-invisible datepicker-selected-text"> [' + pe.dic.get('%datepicker-selected') + ']</span>');
 						}
 					}
 				} catch (e) {
@@ -184,27 +184,27 @@
 			};
 
 			addSelectedDateToField = function (fieldid, year, month, day, format) {
-				wrapper.find("#" + fieldid).attr("value", formatDate(year, month, day, format));
+				wrapper.find('#' + fieldid).attr('value', formatDate(year, month, day, format));
 			};
 
 			toggle = function (fieldid, containerid) {
-				var toggle = wrapper.find("#" + containerid + "-toggle");
-				toggle.toggleClass("picker-toggle-hidden picker-toggle-visible");
+				var toggle = wrapper.find('#' + containerid + '-toggle');
+				toggle.toggleClass('picker-toggle-hidden picker-toggle-visible');
 
-				container.unbind("focusout.calendar");
-				container.unbind("focusin.calendar");
+				container.unbind('focusout.calendar');
+				container.unbind('focusin.calendar');
 
-				if (toggle.hasClass("picker-toggle-visible")) {
+				if (toggle.hasClass('picker-toggle-visible')) {
 					//Hide all other calendars
 					hideAll(fieldid);
 
 					//Enable the tabbing of all the links when calendar is visible
-					container.find("a").attr("tabindex", 0);
+					container.find('a').attr('tabindex', 0);
 					container.slideDown('fast', function () {
 						ieFix($(this));
 					});
-					container.attr("aria-hidden", "false");
-					toggle.children("a").children("span").text(pe.dic.get('%datepicker-hide'));
+					container.attr('aria-hidden', 'false');
+					toggle.children('a').children('span').text(pe.dic.get('%datepicker-hide'));
 
 					if (container.find('.cal-prevmonth a').length !== 0) {
 						pe.focus(container.find('.cal-prevmonth a'));
@@ -217,13 +217,13 @@
 					}
 				} else {
 					hide($('#' + fieldid));
-					pe.focus(wrapper.find("#" + fieldid));
+					pe.focus(wrapper.find('#' + fieldid));
 				}
 			};
 
 			hideAll = function (exception) {
 				$('.picker-field').each(function () {
-					if ($(this).attr("id") !== exception) {
+					if ($(this).attr('id') !== exception) {
 						hide($(this));
 					}
 				});
@@ -232,20 +232,20 @@
 			hide = function (pickerField) {
 				var fieldid, containerid, container, toggle, fieldLabel;
 
-				fieldid = pickerField.attr("id");
+				fieldid = pickerField.attr('id');
 				containerid = fieldid + '-picker';
 				container = $('#' + containerid);
-				toggle = $("#" + containerid + "-toggle");
-				fieldLabel = $("label[for='" + fieldid + "']").text();
+				toggle = $('#' + containerid + '-toggle');
+				fieldLabel = $('label[for="' + fieldid + '"]').text();
 
 				//Disable the tabbing of all the links when calendar is hidden
-				container.find("a").attr("tabindex", "-1");
+				container.find('a').attr('tabindex', '-1');
 				container.slideUp('fast', function () { ieFix($(this)); });
-				container.attr("aria-hidden", "true");
+				container.attr('aria-hidden', 'true');
 				calendar.hideGoToForm(containerid);
-				toggle.children("a").children("span").text(pe.dic.get('%datepicker-show') + fieldLabel);
-				toggle.removeClass("picker-toggle-visible");
-				toggle.addClass("picker-toggle-hidden");
+				toggle.children('a').children('span').text(pe.dic.get('%datepicker-show') + fieldLabel);
+				toggle.removeClass('picker-toggle-visible');
+				toggle.addClass('picker-toggle-hidden');
 			};
 
 			ieFix = function (container) {
@@ -255,47 +255,47 @@
 				if (pe.ie === 7) {
 					calendarBottom = container.height() + container.offset().top - wbMainIn.offset().top + 50;
 					if (wbMainIn.height() >= calendarBottom) {
-						wbMainIn.css("min-height", "");
+						wbMainIn.css('min-height', '');
 					} else if (wbMainIn.height() < calendarBottom) {
-						wbMainIn.css("min-height", calendarBottom);
+						wbMainIn.css('min-height', calendarBottom);
 					}
 				} else if (pe.ie > 0 && pe.ie < 7) {
 					calendarBottom = container.height() + container.offset().top - wbMainIn.offset().top + 50;
 					if (wbMainIn.height() >= calendarBottom) {
-						wbMainIn.css("height", "");
+						wbMainIn.css('height', '');
 					} else if (wbMainIn.height() < calendarBottom) {
-						wbMainIn.css("height", calendarBottom);
+						wbMainIn.css('height', calendarBottom);
 					}
 				}
 			};
 
 			formatDate = function (year, month, day, format) {
 				var output = format;
-				output = output.replace("DD", calendar.strPad(day, 2, '0'));
-				output = output.replace("D", day);
-				output = output.replace("MM", calendar.strPad(month, 2, '0'));
-				output = output.replace("M", month);
-				output = output.replace("YYYY", year);
-				output = output.replace("YY", year.toString().substr(2, 2));
+				output = output.replace('DD', calendar.strPad(day, 2, '0'));
+				output = output.replace('D', day);
+				output = output.replace('MM', calendar.strPad(month, 2, '0'));
+				output = output.replace('M', month);
+				output = output.replace('YYYY', year);
+				output = output.replace('YY', year.toString().substr(2, 2));
 
 				return output;
 			};
 
-			if (elm.attr("id") !== undefined) {
-				id = elm.attr("id");
-				if (elm.attr("data-min-date") !== undefined) {
-					minDate = elm.attr("data-min-date");
+			if (elm.attr('id') !== undefined) {
+				id = elm.attr('id');
+				if (elm.attr('data-min-date') !== undefined) {
+					minDate = elm.attr('data-min-date');
 				} else {
 					minDate = '1800-01-01';
 				}
 
-				if (elm.attr("data-max-date") !== undefined) {
-					maxDate = elm.attr("data-max-date");
+				if (elm.attr('data-max-date') !== undefined) {
+					maxDate = elm.attr('data-max-date');
 				} else {
 					maxDate = '2100-01-01';
 				}
 
-				id = elm.attr("id");
+				id = elm.attr('id');
 				field = elm;
 				wrapper = elm.parent();
 				containerid = id + '-picker';
@@ -311,12 +311,12 @@
 
 				field.parent().after(container);
 
-				container.on("calendarDisplayed", function (e, year,  month, days) {
+				container.on('calendarDisplayed', function (e, year,  month, days) {
 					var $this = $(this);
 					addLinksToCalendar(id, year, month, days, minDate, maxDate, format);
 					setSelectedDate(id, year, month, days, format);
 
-					$this.on("click vclick", function (e) {
+					$this.on('click vclick', function (e) {
 						if (e.stopPropagation) {
 							e.stopImmediatePropagation();
 						} else {
@@ -325,14 +325,14 @@
 					});
 
 					// Close the popup a second after blur
-					$this.on("focusoutside", function () {
+					$this.on('focusoutside', function () {
 						if (container.attr('aria-hidden') === 'false') {
 							hide($('#' + container.attr('id').slice(0, -7)));
 							return false;
 						}
 					});
 
-					$(document).on("click touchstart", function () {
+					$(document).on('click touchstart', function () {
 						if (container.attr('aria-hidden') === 'false') {
 							hide($('#' + container.attr('id').slice(0, -7)));
 							return false;
@@ -344,7 +344,7 @@
 				createToggleIcon(id, containerid);
 
 				//Disable the tabbing of all the links when calendar is hidden
-				container.find("a").attr("tabindex", "-1");
+				container.find('a').attr('tabindex', '-1');
 			}
 		});
 	};
