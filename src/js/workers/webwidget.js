9fa36a2e44bba2e37771997b737194c76e5eb027
Laurent Goderre <laurent.goderre@gmail.com>
2012-08-29 12:39:21 -0400

    Fixed the use of quotes and double quotes for the web feeds widget

--- src/js/workers/webwidget.js
+++ src/js/workers/webwidget.js
@@ -19,40 +19,40 @@
 				var cap, i, result, sorted;
 				cap = (limit > 0 && limit < entries.length ? limit : entries.length);
 				sorted = entries.sort(function (a, b) {
-					return pe.date.compare(b.created_at.replace("+0000 ", "") + " GMT", a.created_at.replace("+0000 ", "") + " GMT");
+					return pe.date.compare(b.created_at.replace('+0000 ', '') + ' GMT', a.created_at.replace('+0000 ', '') + ' GMT');
 				});
-				result = "<ul class=\"widget-content\">";
+				result = '<ul class="widget-content">';
 				i = 0;
 				while (i < cap) {
-					result += "<li><a class=\"float-left\" href=\"http://www.twitter.com/";
-					result += sorted[i].user.name + "/status/" + sorted[i].user.id + "\">" + "<img class=\"widget-avatar\" src=\"" + sorted[i].user.profile_image_url + "\" alt=\"" + sorted[i].user.name + "\" /></a> ";
+					result += '<li><a class="float-left" href="http://www.twitter.com/';
+					result += sorted[i].user.name + '/status/' + sorted[i].user.id + '"><img class="widget-avatar" src="' + sorted[i].user.profile_image_url + '" alt="' + sorted[i].user.name + '" /></a> ';
 					result += pe.string.ify.clean(sorted[i].text);
-					result += " <span class=\"widget-datestamp-accent\">" + pe.dic.ago(sorted[i].created_at) + "</span></li>";
+					result += ' <span class="widget-datestamp-accent">' + pe.dic.ago(sorted[i].created_at) + '</span></li>';
 					i += 1;
 				}
-				result += "</ul>";
+				result += '</ul>';
 				return elm.replaceWith(result);
 			},
 			_map_entries : function (data) {
 				return data;
 			},
 			_json_request: function (url) {
-				if (url.toLowerCase().indexOf("!/search/") > -1) {
-					return url.replace("http://", "https://").replace(/https:\/\/twitter.com\/#!\/search\/(.+$)/, function (str, p1) {
-						return "http://search.twitter.com/search.json?q=" + encodeURI(decodeURI(p1));
+				if (url.toLowerCase().indexOf('!/search/') > -1) {
+					return url.replace('http://', 'https://').replace(/https:\/\/twitter.com\/#!\/search\/(.+$)/, function (str, p1) {
+						return 'http://search.twitter.com/search.json?q=' + encodeURI(decodeURI(p1));
 					});
 				}
-				return url.replace("http://", "https://").replace(/https:\/\/twitter.com\/#!\/(.+$)/i, function (str, p1) {
-					return "http://twitter.com/status/user_timeline/" + encodeURI(decodeURI(p1)) + ".json?callback=?";
+				return url.replace('http://', 'https://').replace(/https:\/\/twitter.com\/#!\/(.+$)/i, function (str, p1) {
+					return 'http://twitter.com/status/user_timeline/' + encodeURI(decodeURI(p1)) + '.json?callback=?';
 				});
 			}
 		},
 		weather: {
 			_parse_entries: function (entries, limit, elm) {
 				var result;
-				result = "<ul class=\"widget-content\">";
-				result += "<li><a href=\"" + entries[1].link + "\">" + entries[1].title + "</a><span class=\"widget-datestamp\">[" + pe.date.to_iso_format(entries[1].publishedDate, true) + "]</span></li>";
-				result += "</ul>";
+				result = '<ul class="widget-content">';
+				result += '<li><a href="' + entries[1].link + '">' + entries[1].title + '</a><span class="widget-datestamp">[' + pe.date.to_iso_format(entries[1].publishedDate, true) + ']</span></li>';
+				result += '</ul>';
 				return elm.replaceWith(result);
 			},
 			_map_entries : function (data) {
@@ -60,8 +60,8 @@
 			},
 			_json_request: function (url, limit) {
 				var rl;
-				url = url.replace(/^.*?\.gc\.ca\/([a-z]+).+\/(.*?)_[a-z]+_([ef])\.html/i, "http://www.weatheroffice.gc.ca/rss/$1/$2_$3.xml");
-				rl = "http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&callback=?&q=" + encodeURI(decodeURI(url));
+				url = url.replace(/^.*?\.gc\.ca\/([a-z]+).+\/(.*?)_[a-z]+_([ef])\.html/i, 'http://www.weatheroffice.gc.ca/rss/$1/$2_$3.xml');
+				rl = 'http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&callback=?&q=' + encodeURI(decodeURI(url));
 				if (limit > 0) {
 					rl += "&num=" + limit;
 				}
@@ -75,13 +75,13 @@
 				sorted = entries.sort(function (a, b) {
 					return pe.date.compare(b.publishedDate, a.publishedDate);
 				});
-				result = "<ul class=\"widget-content\">";
+				result = '<ul class="widget-content">';
 				i = 0;
 				while (i < cap) {
-					result += "<li><a href=\"" + sorted[i].link + "\">" + sorted[i].title + "</a><span class=\"widget-datestamp\">[" + pe.date.to_iso_format(sorted[i].publishedDate, true) + "]</span></li>";
+					result += '<li><a href="' + sorted[i].link + '">' + sorted[i].title + '</a><span class="widget-datestamp">[' + pe.date.to_iso_format(sorted[i].publishedDate, true) + ']</span></li>';
 					i += 1;
 				}
-				result += "</ul>";
+				result += '</ul>';
 				return elm.replaceWith(result);
 			},
 
@@ -90,9 +90,9 @@
 			},
 			_json_request: function (url, limit) {
 				var rl;
-				rl = "http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&callback=?&q=" + encodeURI(decodeURI(url));
+				rl = 'http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&callback=?&q=' + encodeURI(decodeURI(url));
 				if (limit > 0) {
-					rl += "&num=" + limit;
+					rl += '&num=' + limit;
 				}
 				return rl;
 			}
@@ -115,8 +115,8 @@
 				}
 				return $(this).attr("href");
 			});
-			$response = $("<ul class=\"widget-content\"><li class=\"widget-state-loading\"><img src=\"" + pe.add.liblocation + "/images/webfeeds/ajax-loader.gif" + "\" alt=\"" + pe.dic.get('%loading') + "\" /></li></ul>");
-			$elm.find(".widget-content").replaceWith($response);
+			$response = $('<ul class="widget-content"><li class="widget-state-loading"><img src="' + pe.add.liblocation + '/images/webfeeds/ajax-loader.gif" alt="' + pe.dic.get('%loading') + '" /></li></ul>');
+			$elm.find('.widget-content').replaceWith($response);
 
 			typeObj = _pe.fn.webwidget[type];
 
