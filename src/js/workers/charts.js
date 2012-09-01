7c3a3ac08b1b53f1d88a3a063849debe15bac333
Pierre Dubois <pierre.dubois@ic.gc.ca>
2012-08-31 21:46:28 -0500

    chart modified rebuild

--- src/js/workers/charts.js
+++ src/js/workers/charts.js
@@ -3,7 +3,7 @@
 * www.tbs.gc.ca/ws-nw/wet-boew/terms / www.sct.gc.ca/ws-nw/wet-boew/conditions
 */
 /*
-* Chart  functionality WET
+* Chart for WET 3.0
 */
 /*global jQuery: false, pe:false, wet_boew_charts: false, Raphael: false*/
 (function ($) {
@@ -51,26 +51,12 @@
 				// Note: Any Top or bottom value will be overwritten with an pre-defined interval
 				"steps-autocreate": true,
 				"steps-typeof": "number",
-				// "wb-graph-topvalue"-[Number]
-				// wb-graph-topvaluenegative-[true | false]
-				// wb-graph-bottomvalue-[Number]
-				// wb-graph-bottomvaluenegative-[true | false]
 				// This is to set a decimal precision for the pie chart
 				"decimal-autocreate": true,
 				"decimal-typeof": "number",
 				// This is to set the delayed execution timer
 				"execdelay-autocreate": true,
 				"execdelay-typeof": "number",
-				/*
-			general: {
-				type: 'line', // line, bar, stacked, pie
-				width: $(this).width(), // This will be re-adjusted to the best fit as a lower value
-				height: $(this).height(), // This will be re-adjusted to the best fit as a lower value
-				maxWidth: 598, // Will overwrite the 'width' value if the width are larger
-				parseDirection: 'x', // 'x' | 'y' => That is the way to inteprete the table serie if is vertical or horizontal
-				foregroundColor: 'black', // Color used to draw the label and line (HTML color name or hexa Color value like #000000
-				hide: false // Indicate if the Table need to be hidden
-			},*/
 				// This is the default option for a series
 				serie: {
 					type: 'line',
@@ -173,23 +159,6 @@
 					padding: 4, // Number of pixel, distance between the axes/graph limit and the text
 					minNbIncrementStep: 6 // Minimum Number of incrementing step, that's mean an auto resize if needed and no Axis cut
 				}
-				// // //
-				// Not longer supported Options
-				// // //
-				// For acessibility and comprehension, a caption is always required
-				// appendTitle: true, // table caption text is added to chart
-				// title: null, // grabs from table caption if null
-				// // //
-				// Because the Key (header) of the serie are invisible the key (legend) is always added
-				// appendKey: true, //color key is added to chart
-				// // //
-				// In a future version that would be replaced by th class attribute to specify the column and/or row are descriptive only
-				// rowFilter: ' ',
-				// colFilter: ' ',
-				// // //
-				// Replaced by font.height
-				// yLabelInterval: 30 //distance between y labels
-				// // //
 			}, options);
 			function colourNameToHex(colour) {
 				var colours = {"aliceblue": "#f0f8ff", "antiquewhite": "#faebd7", "aqua": "#00ffff", "aquamarine": "#7fffd4", "azure": "#f0ffff", "beige": "#f5f5dc", "bisque": "#ffe4c4", "black": "#000000", "blanchedalmond": "#ffebcd", "blue": "#0000ff", "blueviolet": "#8a2be2", "brown": "#a52a2a", "burlywood": "#deb887", "cadetblue": "#5f9ea0", "chartreuse": "#7fff00", "chocolate": "#d2691e", "coral": "#ff7f50", "cornflowerblue": "#6495ed", "cornsilk": "#fff8dc", "crimson": "#dc143c", "cyan": "#00ffff", "darkblue": "#00008b", "darkcyan": "#008b8b", "darkgoldenrod": "#b8860b", "darkgray": "#a9a9a9", "darkgreen": "#006400", "darkkhaki": "#bdb76b", "darkmagenta": "#8b008b", "darkolivegreen": "#556b2f", "darkorange": "#ff8c00", "darkorchid": "#9932cc", "darkred": "#8b0000", "darksalmon": "#e9967a", "darkseagreen": "#8fbc8f", "darkslateblue": "#483d8b", "darkslategray": "#2f4f4f", "darkturquoise": "#00ced1", "darkviolet": "#9400d3", "deeppink": "#ff1493", "deepskyblue": "#00bfff", "dimgray": "#696969", "dodgerblue": "#1e90ff", "firebrick": "#b22222", "floralwhite": "#fffaf0", "forestgreen": "#228b22", "fuchsia": "#ff00ff", "gainsboro": "#dcdcdc", "ghostwhite": "#f8f8ff", "gold": "#ffd700", "goldenrod": "#daa520", "gray": "#808080", "green": "#008000", "greenyellow": "#adff2f", "honeydew": "#f0fff0", "hotpink": "#ff69b4", "indianred ": "#cd5c5c", "indigo ": "#4b0082", "ivory": "#fffff0", "khaki": "#f0e68c", "lavender": "#e6e6fa", "lavenderblush": "#fff0f5", "lawngreen": "#7cfc00", "lemonchiffon": "#fffacd", "lightblue": "#add8e6", "lightcoral": "#f08080", "lightcyan": "#e0ffff", "lightgoldenrodyellow": "#fafad2", "lightgrey": "#d3d3d3", "lightgreen": "#90ee90", "lightpink": "#ffb6c1", "lightsalmon": "#ffa07a", "lightseagreen": "#20b2aa", "lightskyblue": "#87cefa", "lightslategray": "#778899", "lightsteelblue": "#b0c4de", "lightyellow": "#ffffe0", "lime": "#00ff00", "limegreen": "#32cd32", "linen": "#faf0e6", "magenta": "#ff00ff", "maroon": "#800000", "mediumaquamarine": "#66cdaa", "mediumblue": "#0000cd", "mediumorchid": "#ba55d3", "mediumpurple": "#9370d8", "mediumseagreen": "#3cb371", "mediumslateblue": "#7b68ee", "mediumspringgreen": "#00fa9a", "mediumturquoise": "#48d1cc", "mediumvioletred": "#c71585", "midnightblue": "#191970", "mintcream": "#f5fffa", "mistyrose": "#ffe4e1", "moccasin": "#ffe4b5", "navajowhite": "#ffdead", "navy": "#000080", "oldlace": "#fdf5e6", "olive": "#808000", "olivedrab": "#6b8e23", "orange": "#ffa500", "orangered": "#ff4500", "orchid": "#da70d6", "palegoldenrod": "#eee8aa", "palegreen": "#98fb98", "paleturquoise": "#afeeee", "palevioletred": "#d87093", "papayawhip": "#ffefd5", "peachpuff": "#ffdab9", "peru": "#cd853f", "pink": "#ffc0cb", "plum": "#dda0dd", "powderblue": "#b0e0e6", "purple": "#800080", "red": "#ff0000", "rosybrown": "#bc8f8f", "royalblue": "#4169e1", "saddlebrown": "#8b4513", "salmon": "#fa8072", "sandybrown": "#f4a460", "seagreen": "#2e8b57", "seashell": "#fff5ee", "sienna": "#a0522d", "silver": "#c0c0c0", "skyblue": "#87ceeb", "slateblue": "#6a5acd", "slategray": "#708090", "snow": "#fffafa", "springgreen": "#00ff7f", "steelblue": "#4682b4", "tan": "#d2b48c", "teal": "#008080", "thistle": "#d8bfd8", "tomato":  "#ff6347", "turquoise":  "#40e0d0", "violet":  "#ee82ee", "wheat":  "#f5deb3", "white":  "#ffffff", "whitesmoke":  "#f5f5f5", "yellow": "#ffff00", "yellowgreen": "#9acd32"};
@@ -2387,6 +2356,7 @@ label:
 			//
 			// END of Performance Strategy
 			//
+
 			//
 			// Type of serie and graph in general
 			//
@@ -2395,13 +2365,6 @@ label:
 			//	wb-graph-type-stacked : Same as the Bar but if the previous serie are stacked [line or pie are ignored] would be at the same place over it
 			//	wb-graph-type-pie : Pie Chart for that serie [The Pie need to have it's own sqare space]
 
-			// NOTE:
-			// class example: wet-graph-color-[color partern]-percent
-			// wet-graph-color-rgb255000000 => rgb(255,0,0) (not work)
-			// wet-graph-color-red => red
-			// wet-graph-color-f00 => #f00 (not work)
-			// wet-graph-color-ffffff => #ffffff (not work)
-			// wet-graph-line
 			//
 			// Old parser
 			//
