1851b4984828f6bb31977f2550fe0843ca642c4c
Pierre Dubois <pierre.dubois@ic.gc.ca>
2012-10-04 20:39:25 -0500

    Fixed the zebra to manage more than one complex table in the same page without requesting two time the table parser dependency

--- src/js/workers/zebra.js
+++ src/js/workers/zebra.js
@@ -14,231 +14,232 @@
 	/* local reference */
 	_pe.fn.zebra = {
 		type: 'plugin',
+		fnZebraComplexTable: function (elem, opts) {
+
+			var tblparser,
+				i,
+				j,
+				getCellHeaders,
+				autoRemoveTimeout,
+				$trs,
+				$cols;
+
+			// Prevent multi call issue on dependency load
+			if (!_pe.fn.parsertable) {
+				$(elem).css('background-color', 'red');
+				return;
+			}
+
+			// Parse the table
+			if (!$(elem).data().tblparser) {
+				_pe.fn.parsertable.parse($(elem));
+			}
+			tblparser = $(elem).data().tblparser; // Create an alias
+			// Key Cell
+			if (tblparser.keycell) {
+				for (i = 0; i < tblparser.keycell.length; i += 1) {
+					$(tblparser.keycell[i].elem).addClass('table-keycell');
+				}
+			}
+			// Description Cell
+			if (tblparser.desccell) {
+				for (i = 0; i < tblparser.desccell.length; i += 1) {
+					$(tblparser.desccell[i].elem).addClass('table-desccell');
+				}
+			}
+			// Layout Cell
+			if (tblparser.layoutCell) {
+				for (i = 0; i < tblparser.layoutCell.length; i += 1) {
+					$(tblparser.layoutCell[i].elem).addClass('table-layoutCell');
+				}
+			}
+			// Summary Cell
+			if (tblparser.row) {
+				for (i = 0; i < tblparser.row.length; i += 1) {
+					for (j = 0; j < tblparser.row[i].cell.length; j += 1) {
+						if (tblparser.row[i].cell[j].type === 3) {
+							if (tblparser.row[i].cell[j].col.type === 3) {
+								$(tblparser.row[i].cell[j].elem).addClass('table-summarycol' + tblparser.row[i].cell[j].collevel); // collevel is a number
+							}
+							if (tblparser.row[i].type === 3) {
+								$(tblparser.row[i].cell[j].elem).addClass('table-summaryrow' + tblparser.row[i].cell[j].rowlevel); // rowlevel is a number
+								if (tblparser.row[i].level === 0 && tblparser.row[i].header.elem) {
+									// Special case for heading in that row
+									$(tblparser.row[i].header.elem).addClass('table-summaryrow' + tblparser.row[i].cell[j].rowlevel); // rowlevel is a number
+								}
+							}
+						}
+					}
+					// Summary group styling
+					if (tblparser.row[i].type && tblparser.row[i].type === 3 && tblparser.row[i].rowgroup.elem && i > 0 && tblparser.row[i - 1].type && tblparser.row[i - 1].type === 3 && tblparser.row[i - 1].rowgroup.uid !== tblparser.row[i].rowgroup.uid) {
+						$(tblparser.row[i].rowgroup.elem).addClass('table-rowgroupmarker');
+					}
+				}
+			}
+			// Header Group
+			$('th', elem).each(function () {
+				var $this = $(this);
+				if ($this.data().tblparser.type === 7) {
+					$this.addClass('table-headgroup' + $this.data().tblparser.scope + $this.data().tblparser.level);  // level is a number, scope either "row" || "col"
+				}
+			});
+
+			// Data Column Group
+			if (tblparser.colgroup) {
+				for (i = 0; i < tblparser.colgroup.length; i += 1) {
+					if (tblparser.colgroup[i].elem && ((i > 0 && tblparser.colgroup[i].type === 3 && tblparser.colgroup[i - 1].type === 3 && tblparser.colgroup[i - 1].level > tblparser.colgroup[i].level) ||
+						(tblparser.colgroup[i].type === 2 && ((i > 0 && tblparser.colgroup[0].type === 2) || (i > 1 && tblparser.colgroup[0].type === 1))))) {
+						$(tblparser.colgroup[i].elem).addClass('table-colgroupmarker');
+					}
+				}
+			}
+
+			// Data Row Group
+			if (tblparser.lstrowgroup) {
+				for (i = 0; i < tblparser.lstrowgroup.length; i += 1) {
+					if (tblparser.lstrowgroup[i].elem && tblparser.lstrowgroup[i].type === 2 && i > 0) {
+						$(tblparser.lstrowgroup[i].elem).addClass('table-rowgroupmarker');
+					}
+				}
+			}
+
+			/* The Heading highlight take times to be set up in ÌE and just a little bit more in Firefox
+			*
+			*/
+			if (!opts.noheaderhighlight || opts.columnhighlight) {
+				getCellHeaders = function (elem) {
+					var cellsheader = [],
+						tblparser = $(elem).data().tblparser;
+					if (tblparser.row && tblparser.row.header && !opts.norowheaderhighlight) {
+						for (i = 0; i < tblparser.row.header.length; i += 1) {
+							cellsheader.push(tblparser.row.header[i].elem);
+						}
+						if (tblparser.addrowheaders) {
+							for (i = 0; i < tblparser.addrowheaders.length; i += 1) {
+								cellsheader.push(tblparser.addrowheaders[i].elem);
+							}
+						}
+					}
+					if (tblparser.col && tblparser.col.header && !opts.nocolheaderhighlight) {
+						for (i = 0; i < tblparser.col.header.length; i += 1) {
+							cellsheader.push(tblparser.col.header[i].elem);
+						}
+						if (tblparser.addcolheaders) {
+							for (i = 0; i < tblparser.addcolheaders.length; i += 1) {
+								cellsheader.push(tblparser.addcolheaders[i].elem);
+							}
+						}
+					}
+					$(elem).data().cellsheader = cellsheader;
+				};
+
+				// Cell Header Highlight
+				$('td, th', elem).on('mouseenter focusin', function () {
+					var tblparser = $(this).data().tblparser,
+						oldThHover,
+						$this = $(this);
+					if (!opts.noheaderhighlight) {
+						clearTimeout(autoRemoveTimeout);
+						oldThHover = $('th.table-hover', elem);
+						if (tblparser.type !== 1) {
+							if (!$this.data().cellsheader) {
+								getCellHeaders(this);
+							}
+							//$($this.data().cellsheader).addClass('table-hover');
+							$.each($this.data().cellsheader, function () {
+								var $cheader = $(this);
+								$cheader.addClass('table-hover');
+								$cheader.data().zebrafor = tblparser.uid;
+							});
+						} else {
+							if (tblparser.scope === "row" && !opts.norowheaderhighlight) {
+								$this.addClass('table-hover');
+								$this.data().zebrafor = tblparser.uid;
+							}
+						}
+						// Remove previous highlight, if required
+						$.each(oldThHover, function () {
+							var $old = $(this);
+							if ($old.data().zebrafor && $old.data().zebrafor !== tblparser.uid) {
+								$old.removeClass('table-hover');
+								delete $old.data().zebrafor;
+							}
+						});
+					}
+					if (opts.columnhighlight && tblparser.col && tblparser.col.elem) {
+						$(tblparser.col.elem).addClass('table-hover');
+					}
+				});
+				$('td, th', elem).on('mouseleave focusout', function () {
+					var tblparser = $(this).data().tblparser,
+						elem = this;
+					if (!opts.noheaderhighlight) {
+						autoRemoveTimeout = setTimeout(function () {
+							var i;
+							if (tblparser.type === 1) {
+								$(elem).removeClass('table-hover');
+								delete $(elem).data().zebrafor;
+								return;
+							}
+							for (i = 0; i < $(elem).data().cellsheader.length; i += 1) {
+								if ($($(elem).data().cellsheader[i]).data().zebrafor === tblparser.uid) {
+									$($(elem).data().cellsheader[i]).removeClass('table-hover');
+									delete $($(elem).data().cellsheader[i]).data().zebrafor;
+								}
+							}
+						}, 25);
+					}
+					if (opts.columnhighlight && tblparser.col && tblparser.col.elem) {
+						$(tblparser.col.elem).removeClass('table-hover');
+					}
+				});
+			}
+
+
+			// Default Zebra
+			$trs = (elem.children('tr').add(elem.children('tbody').children('tr'))).filter(function () {
+				return $(this).children('td').length > 0;
+			});
+
+			$trs.on('mouseleave focusout', function (e) {
+				e.stopPropagation();
+				$(this).removeClass('table-hover');
+			});
+			$trs.on('mouseenter focusin', function (e) {
+				e.stopPropagation();
+				$(this).addClass('table-hover');
+			});
+
+			if (opts.vectorstripe) {
+				if (!opts.columnhighlight) {
+					// note: even/odd's indices start at 0
+					$trs.filter(':odd').addClass('table-even');
+					$trs.filter(':even').addClass('table-odd');
+				} else {
+					$cols = [];
+					for (i = 0; i < tblparser.col.length; i += 1) {
+						if (tblparser.col[i].elem) {
+							$cols.push(tblparser.col[i].elem);
+						}
+					}
+					$($cols).filter(':odd').addClass('table-even');
+					$($cols).filter(':even').addClass('table-odd');
+				}
+			}
+
+		},
 		_exec: function (elem) {
 			var $trs,
 				$cols,
 				$lis,
 				parity,
-				tblparser,
 				isSimpleTable = true,
-				fnZebraComplexTable,
-				time = (new Date()).getTime(),
 				i,
-				j,
 				opts,
 				overrides,
-				getCellHeaders,
-				autoRemoveTimeout,
 				lstDlItems = [],
 				isodd = false,
-				dlitem = [],
-				fnZebraComplexTable = function (elem) {
-
-					
-					// Prevent multi call issue on dependency load
-					if (!_pe.fn.parsertable) {
-						$(elem).css('background-color', 'red');
-						return;
-					}
-
-					// Parse the table
-					if (!$(elem).data().tblparser) {
-						_pe.fn.parsertable.parse($(elem));
-					}
-					tblparser = $(elem).data().tblparser; // Create an alias
-					// Key Cell
-					if (tblparser.keycell) {
-						for (i = 0; i < tblparser.keycell.length; i += 1) {
-							$(tblparser.keycell[i].elem).addClass('table-keycell');
-						}
-					}
-					// Description Cell
-					if (tblparser.desccell) {
-						for (i = 0; i < tblparser.desccell.length; i += 1) {
-							$(tblparser.desccell[i].elem).addClass('table-desccell');
-						}
-					}
-					// Layout Cell
-					if (tblparser.layoutCell) {
-						for (i = 0; i < tblparser.layoutCell.length; i += 1) {
-							$(tblparser.layoutCell[i].elem).addClass('table-layoutCell');
-						}
-					}
-					// Summary Cell
-					if (tblparser.row) {
-						for (i = 0; i < tblparser.row.length; i += 1) {
-							for (j = 0; j < tblparser.row[i].cell.length; j += 1) {
-								if (tblparser.row[i].cell[j].type === 3) {
-									if (tblparser.row[i].cell[j].col.type === 3) {
-										$(tblparser.row[i].cell[j].elem).addClass('table-summarycol' + tblparser.row[i].cell[j].collevel); // collevel is a number
-									}
-									if (tblparser.row[i].type === 3) {
-										$(tblparser.row[i].cell[j].elem).addClass('table-summaryrow' + tblparser.row[i].cell[j].rowlevel); // rowlevel is a number
-										if (tblparser.row[i].level === 0 && tblparser.row[i].header.elem) {
-											// Special case for heading in that row
-											$(tblparser.row[i].header.elem).addClass('table-summaryrow' + tblparser.row[i].cell[j].rowlevel); // rowlevel is a number
-										}
-									}
-								}
-							}
-							// Summary group styling
-							if (tblparser.row[i].type && tblparser.row[i].type === 3 && tblparser.row[i].rowgroup.elem && i > 0 && tblparser.row[i - 1].type && tblparser.row[i - 1].type === 3 && tblparser.row[i - 1].rowgroup.uid !== tblparser.row[i].rowgroup.uid) {
-								$(tblparser.row[i].rowgroup.elem).addClass('table-rowgroupmarker');
-							}
-						}
-					}
-					// Header Group
-					$('th', elem).each(function () {
-						var $this = $(this);
-						if ($this.data().tblparser.type === 7) {
-							$this.addClass('table-headgroup' + $this.data().tblparser.scope + $this.data().tblparser.level);  // level is a number, scope either "row" || "col"
-						}
-					});
-
-					// Data Column Group
-					if (tblparser.colgroup) {
-						for (i = 0; i < tblparser.colgroup.length; i += 1) {
-							if (tblparser.colgroup[i].elem && ((i > 0 && tblparser.colgroup[i].type === 3 && tblparser.colgroup[i - 1].type === 3 && tblparser.colgroup[i - 1].level > tblparser.colgroup[i].level) ||
-								(tblparser.colgroup[i].type === 2 && ((i > 0 && tblparser.colgroup[0].type === 2) || (i > 1 && tblparser.colgroup[0].type === 1))))) {
-								$(tblparser.colgroup[i].elem).addClass('table-colgroupmarker');
-							}
-						}
-					}
-
-					// Data Row Group
-					if (tblparser.lstrowgroup) {
-						for (i = 0; i < tblparser.lstrowgroup.length; i += 1) {
-							if (tblparser.lstrowgroup[i].elem && tblparser.lstrowgroup[i].type === 2 && i > 0) {
-								$(tblparser.lstrowgroup[i].elem).addClass('table-rowgroupmarker');
-							}
-						}
-					}
-
-					/* The Heading highlight take times to be set up in ÌE and just a little bit more in Firefox
-					*
-					*/
-					if (!opts.noheaderhighlight || opts.columnhighlight) {
-						getCellHeaders = function (elem) {
-							var cellsheader = [],
-								tblparser = $(elem).data().tblparser;
-							if (tblparser.row && tblparser.row.header && !opts.norowheaderhighlight) {
-								for (i = 0; i < tblparser.row.header.length; i += 1) {
-									cellsheader.push(tblparser.row.header[i].elem);
-								}
-								if (tblparser.addrowheaders) {
-									for (i = 0; i < tblparser.addrowheaders.length; i += 1) {
-										cellsheader.push(tblparser.addrowheaders[i].elem);
-									}
-								}
-							}
-							if (tblparser.col && tblparser.col.header && !opts.nocolheaderhighlight) {
-								for (i = 0; i < tblparser.col.header.length; i += 1) {
-									cellsheader.push(tblparser.col.header[i].elem);
-								}
-								if (tblparser.addcolheaders) {
-									for (i = 0; i < tblparser.addcolheaders.length; i += 1) {
-										cellsheader.push(tblparser.addcolheaders[i].elem);
-									}
-								}
-							}
-							$(elem).data().cellsheader = cellsheader;
-						};
-
-						// Cell Header Highlight
-						$('td, th', elem).on('mouseenter focusin', function () {
-							var tblparser = $(this).data().tblparser,
-								oldThHover,
-								$this = $(this);
-							if (!opts.noheaderhighlight) {
-								clearTimeout(autoRemoveTimeout);
-								oldThHover = $('th.table-hover', elem);
-								if (tblparser.type !== 1) {
-									if (!$this.data().cellsheader) {
-										getCellHeaders(this);
-									}
-									//$($this.data().cellsheader).addClass('table-hover');
-									$.each($this.data().cellsheader, function () {
-										var $cheader = $(this);
-										$cheader.addClass('table-hover');
-										$cheader.data().zebrafor = tblparser.uid;
-									});
-								} else {
-									if (tblparser.scope === "row" && !opts.norowheaderhighlight) {
-										$this.addClass('table-hover');
-										$this.data().zebrafor = tblparser.uid;
-									}
-								}
-								// Remove previous highlight, if required
-								$.each(oldThHover, function () {
-									var $old = $(this);
-									if ($old.data().zebrafor && $old.data().zebrafor !== tblparser.uid) {
-										$old.removeClass('table-hover');
-										delete $old.data().zebrafor;
-									}
-								});
-							}
-							if (opts.columnhighlight && tblparser.col && tblparser.col.elem) {
-								$(tblparser.col.elem).addClass('table-hover');
-							}
-						});
-						$('td, th', elem).on('mouseleave focusout', function () {
-							var tblparser = $(this).data().tblparser,
-								elem = this;
-							if (!opts.noheaderhighlight) {
-								autoRemoveTimeout = setTimeout(function () {
-									var i;
-									if (tblparser.type === 1) {
-										$(elem).removeClass('table-hover');
-										delete $(elem).data().zebrafor;
-										return;
-									}
-									for (i = 0; i < $(elem).data().cellsheader.length; i += 1) {
-										if ($($(elem).data().cellsheader[i]).data().zebrafor === tblparser.uid) {
-											$($(elem).data().cellsheader[i]).removeClass('table-hover');
-											delete $($(elem).data().cellsheader[i]).data().zebrafor;
-										}
-									}
-								}, 25);
-							}
-							if (opts.columnhighlight && tblparser.col && tblparser.col.elem) {
-								$(tblparser.col.elem).removeClass('table-hover');
-							}
-						});
-					}
-
-
-					// Default Zebra
-					$trs = (elem.children('tr').add(elem.children('tbody').children('tr'))).filter(function () {
-						return $(this).children('td').length > 0;
-					});
-
-					$trs.on('mouseleave focusout', function (e) {
-						e.stopPropagation();
-						$(this).removeClass('table-hover');
-					});
-					$trs.on('mouseenter focusin', function (e) {
-						e.stopPropagation();
-						$(this).addClass('table-hover');
-					});
-
-					if (opts.vectorstripe) {
-						if (!opts.columnhighlight) {
-							// note: even/odd's indices start at 0
-							$trs.filter(':odd').addClass('table-even');
-							$trs.filter(':even').addClass('table-odd');
-						} else {
-							$cols = [];
-							for (i = 0; i < tblparser.col.length; i += 1) {
-								if (tblparser.col[i].elem) {
-									$cols.push(tblparser.col[i].elem);
-								}
-							}
-							$($cols).filter(':odd').addClass('table-even');
-							$($cols).filter(':even').addClass('table-odd');
-						}
-					}
-
-				};
+				dlitem = [];
 			// Defaults Options
 			opts = {
 				noheaderhighlight: false,
@@ -351,21 +352,38 @@
 
 					}
 
-
 					return; // Simple Table Zebra Striping done
 				}
 
 
-				
-				if (_pe.fn.parsertable) {
-					fnZebraComplexTable(elem);
-				} else {
-					$(document).on('depsTableParserLoaded-' + time, function() {
-						 fnZebraComplexTable(elem);
-					});
 
-					_pe.wb_load({'dep': ['parserTable']}, 'depsTableParserLoaded-' + time);
+
+
+
+				if (_pe.fn.parsertable) {
+					_pe.fn.zebra.fnZebraComplexTable(elem, opts);
+					return;
 				}
+
+				if (_pe.fn.zebra.complexTblStack) {
+					_pe.fn.zebra.complexTblStack.push(elem);
+					_pe.fn.zebra.complexTblOptsStack.push(jQuery.extend(true, {}, opts));
+					return;
+				}
+
+				_pe.fn.zebra.complexTblStack = [];
+				_pe.fn.zebra.complexTblOptsStack = [];
+				_pe.fn.zebra.complexTblStack.push(elem);
+				_pe.fn.zebra.complexTblOptsStack.push(jQuery.extend(true, {}, opts));
+
+
+				$(document).on('depsTableParserLoaded', function () {
+					while (_pe.fn.zebra.complexTblStack.length > 0) {
+						_pe.fn.zebra.fnZebraComplexTable(_pe.fn.zebra.complexTblStack.shift(), _pe.fn.zebra.complexTblOptsStack.shift());
+					}
+				});
+
+				_pe.wb_load({'dep': ['parserTable']}, 'depsTableParserLoaded');
 
 			} else if (elem.is('dl')) {
 				// Create a list based on "dt" element with their one or more "dd" after each of them
