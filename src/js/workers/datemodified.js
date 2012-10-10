996ddf11aac93d13f07d5032e0c0f53a2e533c3a
Nick Schonning <nschonni@gmail.com>
2012-10-10 17:23:23 -0400

    Plugin to use meta date modified value
    Datepicker stuff is included was it was not built in the previous commit

---
+++ src/js/workers/datemodified.js
@@ -0,0 +1,44 @@
+/*
+ * Web Experience Toolkit (WET) / Boîte à outils de l'expérience Web (BOEW)
+ * wet-boew.github.com/wet-boew/License-eng.txt / wet-boew.github.com/wet-boew/Licence-fra.txt
+ */
+/*
+ * Date Modified Plug-in
+ */
+/*global jQuery: false, pe: false*/
+(function ($) {
+	"use strict";
+	var _pe = window.pe || {
+		fn: {}
+	};
+	/* local reference */
+	_pe.fn.datemodified = {
+		type: 'plugin',
+		_exec: function (elm) {
+			var opts,
+				overrides,
+				timeholder =  $('#gcwu-date-mod dd span time'),
+				modifiedMeta = document.getElementsByName('dcterms.modified')[0];
+
+			// Defaults
+			opts = {
+				// Should the Date Modified value be overwritten
+				updateNonEmpty : false
+			};
+
+			if (typeof wet_boew_datemodified !== 'undefined' && wet_boew_datemodified !== null) {
+				$.extend(opts, wet_boew_datemodified);
+			} else {
+				$.extend(opts);
+			}
+			
+			if ( timeholder.text() === '' ||  opts.updateNonEmpty){
+				timeholder.text(modifiedMeta.content);
+			}
+			
+			return false;
+		} // end of exec
+	};
+	window.pe = _pe;
+	return _pe;
+}(jQuery));
