938cc2068587c3e698cdccecbb7e5e57ed8927db
Nick Schonning <nschonni@gmail.com>
2012-10-15 13:47:57 -0400

    Adding language name to dictionary headers

--- src/js/i18n/en.js
+++ src/js/i18n/en.js
@@ -3,7 +3,7 @@
 * wet-boew.github.com/wet-boew/License-eng.txt / wet-boew.github.com/wet-boew/Licence-fra.txt
 */
 /*
------ Dictionary (il8n) ---
+----- English Dictionary (il8n) ---
 */
 /*global jQuery: false, pe: false */
 /*jshint bitwise: false */
