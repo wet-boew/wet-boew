af78693f8114e7736a8849133c768f08c4d89f27
Paul Jackson <Paul.Jackson@tbs-sct.gc.ca>
2012-11-15 22:04:50 -0500

    v3.0.3-a1 build

--- build/js/dependencies/charts-min.js
+++ build/js/dependencies/charts-min.js
@@ -2,7 +2,7 @@
  * Web Experience Toolkit (WET) / Boîte à outils de l'expérience Web (BOEW)
  * wet-boew.github.com/wet-boew/License-eng.txt / wet-boew.github.com/wet-boew/Licence-fra.txt
  *
- * Version: v3.0.2
+ * Version: v3.0.3-a1
  */
 /*!
 * Web Experience Toolkit (WET) / Boîte à outils de l'expérience Web (BOEW)
