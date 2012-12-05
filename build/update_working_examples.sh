96b4fc4a75e8f2c0bf58c431fe4e2317167b6eca
Laurent <laurent.goderre@gmail.com>
2012-12-05 16:16:53 -0500

    Modified working example script to keep previous builds in history

--- build/update_working_examples.sh
+++ build/update_working_examples.sh
@@ -1,8 +1,8 @@
 export REPO="$(pwd | sed s,^/home/travis/builds/,,g)"
 if [ "$TRAVIS_BRANCH" == "master" -a "$REPO" == "wet-boew/wet-boew" ]; then
 	echo "Updating working examples..."
-	git branch -D gh-pages
-	git checkout -B gh-pages
+	git checkout gh-pages
+	git rebase master
 	git add -f dist/.
 	git commit -m "Travis build $TRAVIS_JOB_ID pushed to gh-pages"
 	git push -fq https://${GH_TOKEN}@github.com/${REPO}.git gh-pages > /dev/null
