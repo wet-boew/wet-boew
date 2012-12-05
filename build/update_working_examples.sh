b7116d023c716f1ba0d2c1ad3d929c89a1025dcb
Laurent <laurent.goderre@gmail.com>
2012-12-05 17:33:17 -0500

    Fixed the changes to the built files being overwritten by the checkout of gh-pages

--- build/update_working_examples.sh
+++ build/update_working_examples.sh
@@ -1,8 +1,10 @@
 export REPO="$(pwd | sed s,^/home/travis/builds/,,g)"
 if [ "$TRAVIS_BRANCH" == "master" -a "$REPO" == "wet-boew/wet-boew" ]; then
 	echo "Updating working examples..."
+	git stash
 	git checkout gh-pages
 	git rebase master
+	git stash pop
 	git add -f dist/.
 	git commit -m "Travis build $TRAVIS_JOB_ID pushed to gh-pages"
 	git push -fq https://${GH_TOKEN}@github.com/${REPO}.git gh-pages > /dev/null
