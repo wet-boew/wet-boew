6bec7c724e237fc13fa90d07f6d7f3d730d2f1d9
Laurent <laurent.goderre@gmail.com>
2012-12-05 19:12:04 -0500

    Fixed the working example script

--- build/update_working_examples.sh
+++ build/update_working_examples.sh
@@ -1,9 +1,12 @@
 export REPO="$(pwd | sed s,^/home/travis/builds/,,g)"
 if [ "$TRAVIS_BRANCH" == "master" -a "$REPO" == "wet-boew/wet-boew" ]; then
 	echo "Updating working examples..."
+	git add -f dist/.
 	git stash
+	git fetch
 	git checkout gh-pages
 	git rebase master
+	git rm -r dist/.
 	git stash pop
 	git add -f dist/.
 	git commit -m "Travis build $TRAVIS_JOB_ID pushed to gh-pages"
