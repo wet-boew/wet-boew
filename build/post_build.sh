86473d0dd6234f4f7aa759388a96c94d3a7f92a8
Laurent Goderre <laurent.goderre@gmail.com>
2012-12-07 14:02:24 -0500

    Added functionality to store the 10 latest build for specified branches

---
+++ build/post_build.sh
@@ -0,0 +1,69 @@
+export REPO="$(pwd | sed s,^/home/travis/builds/,,g)"
+declare -a supported_branches=('master' 'v3.0') # List of branches to store build output for
+number_to_keep=10 #Number of build to keep for
+branch='downloads'
+
+git fetch
+
+#Update working example
+if [ "$REPO" == "wet-boew/wet-boew" ]; then
+	if [ "$TRAVIS_BRANCH" == "master" ]; then
+		echo "Updating working examples..."
+
+		git add -f dist/.
+		git stash
+		git checkout gh-pages
+		git rebase --committer-date-is-author-date master
+		git rm -r dist/.
+		git stash pop
+		git add -f dist/.
+		git commit -m "Travis build $TRAVIS_JOB_ID pushed to gh-pages"
+		git push -fq https://${GH_TOKEN}@github.com/${REPO}.git gh-pages > /dev/null
+
+		echo "Finished updating the working examples"
+	fi
+
+	#Update the artifact branch
+	case "${supported_branches[@]}" in  *"$TRAVIS_BRANCH"*)
+		echo "Updating the $branch branch"
+
+		git checkout $branch
+
+		# Create a folder to store the downloads for this branch if it doesn't exist already
+		if [ ! -d "$TRAVIS_BRANCH" ];
+		then
+			mkdir "$TRAVIS_BRANCH"
+		fi
+
+		cd "$TRAVIS_BRANCH"
+
+		#Only keep a certain number of folders (defined by $number_to_keep)
+		if [ $(ls -1 | wc -l) -gt $number_to_keep ];
+		then
+			ls -Qt | awk 'NR>'$number_to_keep | xargs -r rm -rf
+		fi
+
+		#remove the latest prefix to leave place for the new latest
+		if ls -d latest* &> /dev/null ;
+		then
+			for f in latest-*; do
+				file=${f:7}
+				[ ! -f $file ] && mv "$f" $file
+			done
+		fi
+		cd ..
+
+		#Add the latest build files
+		dest="$TRAVIS_BRANCH/latest-$TRAVIS_COMMIT"
+		git archive --format tar --prefix $dest/ --output "output.tar" gh-pages dist
+		tar xf "output.tar" --transform='s,/dist/,/,'
+		rm -r "output.tar" $dest/dist
+
+		#Commit the result
+		git add -f .
+		git commit -m "Travis build $TRAVIS_JOB_ID pushed to $branch"
+		git push -fq https://${GH_TOKEN}@github.com/${REPO}.git $branch > /dev/null
+
+		echo "Finished updating the $branch branch"
+	;; esac
+fi
