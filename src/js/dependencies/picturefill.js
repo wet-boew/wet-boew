1fcca0f37962c460bb85a4496a55813062a25c02
Pat Heard <pat.heard@gmail.com>
2012-12-10 16:11:25 -0500

    Initial working picture plugin (no IE support)

---
+++ src/js/dependencies/picturefill.js
@@ -0,0 +1,80 @@
+/*! Picturefill - Author: Scott Jehl, 2012 | License: MIT/GPLv2 */
+/*
+	Picturefill: A polyfill for proposed behavior of the picture element, which does not yet exist, but should. :)
+	* Notes: 
+		* For active discussion of the picture element, see http://www.w3.org/community/respimg/
+		* While this code does work, it is intended to be used only for example purposes until either:
+			A) A W3C Candidate Recommendation for <picture> is released
+			B) A major browser implements <picture>
+*/ 
+(function( w ){
+	
+	// Enable strict mode
+	"use strict";
+	
+	// Test if `<picture>` is supported natively, if so, exit - no polyfill needed.
+	if ( !!( w.document.createElement( "picture" ) && w.document.createElement( "source" ) && w.HTMLPictureElement ) ){
+		return;
+	}
+	
+	w.picturefill = function() {
+		var ps = w.document.getElementsByTagName( "picture" );
+
+		// Loop the pictures
+		for( var i = 0, il = ps.length; i < il; i++ ){
+			var sources = ps[ i ].getElementsByTagName( "source" ),
+				matches = [];
+
+			// If no sources are found, they're likely erased from the DOM. Try finding them inside comments.
+			if( !sources.length ){
+				var picText =  ps[ i ].innerHTML,
+					frag = w.document.createElement( "div" ),
+					// For IE9, convert the source elements to divs
+					srcs = picText.replace( /(<)source([^>]+>)/gmi, "$1div$2" ).match( /<div[^>]+>/gmi );
+				
+				if(srcs != null) {
+					frag.innerHTML = srcs.join( "" );
+					sources = frag.getElementsByTagName( "div" );
+				}
+			}
+			
+			// See if which sources match
+			for( var j = 0, jl = sources.length; j < jl; j++ ){
+				var media = sources[ j ].getAttribute( "media" );
+				// if there's no media specified, OR w.matchMedia is supported 
+				if( !media || ( w.matchMedia && w.matchMedia( media ).matches ) ){
+					matches.push( sources[ j ] );
+				}
+			}
+			
+			// Find any existing img element in the picture element
+			var picImg = ps[ i ].getElementsByTagName( "img" )[ 0 ];
+			
+			if( matches.length ){
+			
+				if( !picImg ){
+					picImg = w.document.createElement( "img" );
+					picImg.alt = ps[ i ].getAttribute( "alt" );
+					ps[ i ].appendChild( picImg );
+				}
+				
+				picImg.src =  matches.pop().getAttribute( "src" );
+			}
+		}
+	};
+	
+	// Run on resize and domready (w.load as a fallback)	
+	if( w.addEventListener ){
+		w.addEventListener( "resize", w.picturefill, false );
+		w.addEventListener( "DOMContentLoaded", function(){
+			w.picturefill();
+			// Run once only
+			w.removeEventListener( "load", w.picturefill, false );
+		}, false );
+		w.addEventListener( "load", w.picturefill, false );
+	}
+	else if( w.attachEvent ){
+		w.attachEvent( "onload", w.picturefill );
+	}
+	
+})( this );
---
+++ src/js/workers/picture.js
@@ -0,0 +1,40 @@
+/*
+ * Web Experience Toolkit (WET) / Boîte à outils de l'expérience Web (BOEW)
+ * wet-boew.github.com/wet-boew/License-eng.txt / wet-boew.github.com/wet-boew/Licence-fra.txt
+ */
+/*
+ * Picture element plugin
+ */
+(function ($) {
+	"use strict";
+	var _pe = window.pe || {
+		fn : {}
+	};
+	
+	_pe.fn.picture = {
+		type : 'plugin',
+		depends : ['picturefill', 'matchMedia', 'resize'],
+		_exec : function (elm) {	
+			var w = window;
+			
+			//if(_pe.ie === 0) {
+				// Remove the picturefill resize event listener and let pe.resize to do the work
+				if(w.removeEventListener) {
+					w.removeEventListener( "resize", picturefill, false );
+				}
+				_pe.resize(function() {
+					picturefill();
+				});			
+				
+				// Run picturefill
+				picturefill();		
+			//}
+			elm.find('img').css({visibility: 'visible'});
+			
+			return elm;					
+		} // end of exec
+	};
+	window.pe = _pe;
+	return _pe;
+}
+(jQuery));
