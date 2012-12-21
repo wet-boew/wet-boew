77e825d011c44661a0e2b85e9991b2ddc2d30c3e
Laurent Goderre <laurent.goderre@gmail.com>
2012-12-21 10:26:58 -0500

    Reset the branch to fix problems due to the build script

--- dist/js/i18n/formvalid/methods_pt-min.js
+++
@@ -1,5 +0,0 @@
-/*!
- * Localized default methods for the jQuery validation plugin.
- * Locale: PT_BR
- */
-jQuery.extend(jQuery.validator.methods,{date:function(b,a){return this.optional(a)||/^\d\d?\/\d\d?\/\d\d\d?\d?$/.test(b)}});
