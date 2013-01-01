ba8dc3071beb586ae0487047a30096e74fac1129
travis <travis@precise32.(none)>
2013-01-01 06:09:36 +0000

    Travis build 3901105 pushed to v3.0

--- dist/js/settings.js
+++ dist/js/settings.js
@@ -3,7 +3,7 @@
  * Web Experience Toolkit (WET) / Boîte à outils de l'expérience Web (BOEW)
  * wet-boew.github.com/wet-boew/License-eng.txt / wet-boew.github.com/wet-boew/Licence-fra.txt
  *
- * Version: v3.0.4-a1 Build: 2012-12-31 09:17 PM
+ * Version: v3.0.4-a1 Build: 2013-01-01 06:09 AM
  *
  */
 /*
