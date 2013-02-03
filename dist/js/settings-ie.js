c191f14e4b6b7bb989ac0845dcfab6f0251bff94
Travis <travis@travis-ci.org>
2013-02-03 07:28:34 +0000

    Travis build 2040 pushed to master

--- dist/js/settings-ie.js
+++ dist/js/settings-ie.js
@@ -3,7 +3,7 @@
  * Web Experience Toolkit (WET) / Boîte à outils de l'expérience Web (BOEW)
  * wet-boew.github.com/wet-boew/License-eng.txt / wet-boew.github.com/wet-boew/Licence-fra.txt
  *
- * Version: v3.1.0-b2 Build: 2013-02-02 11:13 PM
+ * Version: v3.1.0-b2 Build: 2013-02-03 07:25 AM
  *
  */
 /*
