c0604306f055a71190150b76943d9631a5adb503
Joost van Ulden <jvanulde@nrcan.gc.ca>
2013-02-05 09:58:33 -0800

    Removed overlays

--- src/js/settings.js
+++ src/js/settings.js
@@ -38,47 +38,4 @@ var wet_boew_geomap = {
 		version: '1.1.1',
 		options: { singleTile: false, ratio: 1.0, projection: 'EPSG:3978', fractionalZoom: true }
 	}*/	
-	
-	overlays : [
-				{
-				   title: 'WMS Demo',
-				   type: 'wms',
-				   url: 'http://www2.dmsolutions.ca/cgi-bin/mswms_gmap',
-				   layers: 'bathymetry,land_fn,park,drain_fn,drainage,prov_bound,fedlimit,rail,road,popplace',		   
-				   format: 'image/png',
-				   visible: false
-				},
-				{
-				   title: 'KML Demo',
-				   type: 'kml',
-				   url: 'data/sample.kml',		   
-				   visible: true
-				},
-				{
-				   title: 'ATOM Demo',
-				   type: 'atom',
-				   url: 'data/sample.atom',		   
-				   visible: true
-				},
-				{
-				   title: 'GeoRSS Demo',
-				   type: 'georss',
-				   url: 'data/sample.rss',		   
-				   visible: true
-				},
-				{
-					title: 'JSON Demo',
-					type: 'json',					
-					url: 'http://geogratis.gc.ca/api/en/nrcan-rncan/ess-sst',
-					params: {
-						'alt': 'json'
-					},
-					visible: 'true',
-					root: 'products',
-					mappings: {
-						'title': 'title',
-						'description': 'summary'
-					}
-				}
-			]
 };
