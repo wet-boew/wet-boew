4284c04a548f7ba4b7087c6468041a94babb620f
Laurent Goderre <laurent.goderre@gmail.com>
2013-02-12 17:20:14 -0500

    Added explicit height for the mobile Canada wordmark SVG

--- src/theme-gcwu-fegc/js/theme.js
+++ src/theme-gcwu-fegc/js/theme.js
@@ -151,7 +151,7 @@
 				// Handling for the Canada Wordmark if it exists
 				if (wet_boew_theme.wmms.length !== 0) {
 					node = wet_boew_theme.wmms[0].getElementsByTagName('img')[0];
-					header += '<div class="ui-title"><object type="image/svg+xml" width="90" data="' + node.getAttribute('src').replace('.gif', '-r.svg') + '"><img src="' + node.getAttribute('src').replace('.gif', '-wm.gif') + '" width="90" alt="' + node.getAttribute('alt') + '" /></object></div>';
+					header += '<div class="ui-title"><object type="image/svg+xml" width="90" height="22" data="' + node.getAttribute('src').replace('.gif', '-r.svg') + '"><img src="' + node.getAttribute('src').replace('.gif', '-wm.gif') + '" width="90" alt="' + node.getAttribute('alt') + '" /></object></div>';
 				} else {
 					header += '<div class="ui-title"></div>';
 				}
