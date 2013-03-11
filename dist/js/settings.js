103f11b3b64b23674f625719edc3a041e8b1bdfc
Nick Schonning <nschonni@gmail.com>
2013-03-11 17:18:36 -0400

    Cleanout old 3.1 files left over from gh-1262 Should prevent issues like gh-1514 from reoccuring

--- dist/js/settings.js
+++ dist/js/settings.js
@@ -3,7 +3,7 @@
  * Web Experience Toolkit (WET) / Boîte à outils de l'expérience Web (BOEW)
  * wet-boew.github.com/wet-boew/License-eng.txt / wet-boew.github.com/wet-boew/Licence-fra.txt
  *
- * Version: v3.0.6-development Build: 2013-03-11 03:45 PM
+ * Version: v3.0.6-development Build: 2013-03-11 05:10 PM
  *
  */
 /*
