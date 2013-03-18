378ebc08f63e3d0a591dd643d535006ee895f07d
Paul Jackson <Paul.Jackson@tbs-sct.gc.ca>
2013-03-18 04:54:48 -0400

    Fix to GCWU intranet SVG icons in mobile view. More extends/placeholders in SCSS. Changed pe. to _pe. where feasible.

--- src/js/workers/share.js
+++ src/js/workers/share.js
@@ -5,7 +5,7 @@
 /*
  * Share widget plugin
  */
-/*global jQuery: false, pe:false, wet_boew_share:false */
+/*global jQuery: false, wet_boew_share:false */
 (function ($) {
 	"use strict";
 	var _pe = window.pe || {
@@ -39,25 +39,25 @@
 				sites: [], // List of site IDs or language selectors (lang:xx) or
 					// category selectors (category:xx) to use, empty for all
 				compact: false, // True if a compact presentation should be used, false for full
-				hint: pe.dic.get('%share-text') + pe.dic.get('%share-hint') + pe.dic.get('%new-window'), // Popup hint for links, {s} is replaced by display name
+				hint: _pe.dic.get('%share-text') + _pe.dic.get('%share-hint') + _pe.dic.get('%new-window'), // Popup hint for links, {s} is replaced by display name
 				popup: true, // True to have it popup on demand, false to show always
 				popupTag: 'h2', // Parent tag for the popup link (should be either h2 or h3)
-				popupText: pe.dic.get('%share-text'), // Text for the popup trigger
+				popupText: _pe.dic.get('%share-text'), // Text for the popup trigger
 				includeDisclaimer: true, // True to include the popup disclaimer (at the bottom)
-				popupDisclaimer: pe.dic.get('%share-disclaimer'), // Text for the popup disclaimer
-				hideText: (pe.dic.get('%hide') + " - "), // Text to prepend to the popup trigger when popup is open
+				popupDisclaimer: _pe.dic.get('%share-disclaimer'), // Text for the popup disclaimer
+				hideText: (_pe.dic.get('%hide') + " - "), // Text to prepend to the popup trigger when popup is open
 				addFavorite: false,  // True to add a 'add to favourites' link, false for none
-				favoriteText: pe.dic.get('%favourite'),  // Display name for the favourites link
+				favoriteText: _pe.dic.get('%favourite'),  // Display name for the favourites link
 				addEmail: false, // True to add a 'e-mail a friend' link, false for none
-				emailText: pe.dic.get('%email'), // Display name for the e-mail link
-				emailSubject: pe.dic.get('%share-email-subject'), // The subject for the e-mail
-				emailBody: pe.dic.get('%share-email-body'), // The body of the e-mail,
+				emailText: _pe.dic.get('%email'), // Display name for the e-mail link
+				emailSubject: _pe.dic.get('%share-email-subject'), // The subject for the e-mail
+				emailBody: _pe.dic.get('%share-email-body'), // The body of the e-mail,
 					// use '{t}' for the position of the page title, '{u}' for the page URL,
 					// '{d}' for the description, and '\n' for new lines
-				manualBookmark: pe.dic.get('%share-manual'), // Instructions for manually bookmarking the page
+				manualBookmark: _pe.dic.get('%share-manual'), // Instructions for manually bookmarking the page
 				addShowAll: false, // True to show listed sites first, then all on demand
-				showAllText: pe.dic.get('%share-showall'), // Display name for show all link, use '{n}' for the number of sites
-				showAllTitle: pe.dic.get('%share-showall-title'), // Title for show all popup
+				showAllText: _pe.dic.get('%share-showall'), // Display name for show all link, use '{n}' for the number of sites
+				showAllTitle: _pe.dic.get('%share-showall-title'), // Title for show all popup
 				addAnalytics: false, // True to include Google Analytics for links
 				analyticsName: '/share/{r}/{s}' // The "URL" that is passed to the Google Analytics,
 					// use '{s}' for the site code, '{n}' for the site name,
@@ -79,7 +79,7 @@
 			$.extend(opts, (typeof wet_boew_share !== 'undefined' ? wet_boew_share : {}), overrides, _pe.data.getData(elm, 'wet-boew'));
 
 			elm.bookmark(opts);
-			if (opts.popup && pe.cssenabled) {
+			if (opts.popup && _pe.cssenabled) {
 				elm.attr('role', 'application');
 				if (opts.popupTag.substring(0, 1) === 'h') { // If a heading element is used for the popup tag, then wrap the contents in a section element
 					elm.wrapInner('<section />');
@@ -110,10 +110,10 @@
 				}
 				if (opts.addEmail) { // Removes target attribute and opens in new window warning from email link
 					match = $popup.find('a[href*="mailto:"]').removeAttr('target').removeAttr('rel');
-					match.attr('title', match.attr('title').replace(pe.dic.get('%new-window'), ''));
+					match.attr('title', match.attr('title').replace(_pe.dic.get('%new-window'), ''));
 				}
 				if (opts.addFavorite) { // Removes target attribute and makes title more relevant for favorite link
-					match = $popup.find('a[href*="#"]').removeAttr('target').removeAttr('rel').attr('title', opts.favoriteText + pe.dic.get('%share-fav-title'));
+					match = $popup.find('a[href*="#"]').removeAttr('target').removeAttr('rel').attr('title', opts.favoriteText + _pe.dic.get('%share-fav-title'));
 				}
 				if (opts.includeDisclaimer) { // Append the disclaimer
 					$popup.append('<p class="popup_disclaimer">' + opts.popupDisclaimer + '</p>');
@@ -169,7 +169,7 @@
 								if (target.length === 0) {
 									target = $popupLinks;
 								}
-								pe.focus(target.last());
+								_pe.focus(target.last());
 								return false;
 							} else if (keyCode === 38) { // up arrow (go one link up, or to the bottom-most link in the previous column, or to the bottom-most link of the last column)
 								leftoffset = e.target.offsetLeft;
@@ -177,22 +177,22 @@
 									return (this.offsetLeft === leftoffset);
 								});
 								if (target.length > 0) {
-									pe.focus(target.first());
+									_pe.focus(target.first());
 								} else {
 									target = $popupLinks.filter(function () {
 										return (this.offsetLeft < leftoffset);
 									});
 									if (target.length > 0) {
-										pe.focus(target.last());
+										_pe.focus(target.last());
 									} else {
 										leftoffset = popupLinksDOM[popupLinksDOM.length - 1].offsetLeft;
 										target = $popupLinks.filter(function () {
 											return (this.offsetLeft > leftoffset);
 										});
 										if (target.length > 0) {
-											pe.focus(target.last());
+											_pe.focus(target.last());
 										} else {
-											pe.focus($popupLinks.last());
+											_pe.focus($popupLinks.last());
 										}
 									}
 								}
@@ -202,7 +202,7 @@
 								if (target.length === 0) {
 									target = $popupLinks;
 								}
-								pe.focus(target.first());
+								_pe.focus(target.first());
 								return false;
 							} else if (keyCode === 40) { // down arrow (go one link down, or to the top-most link in the next column, or to the top-most link of the first column)
 								leftoffset = e.target.offsetLeft;
@@ -210,15 +210,15 @@
 									return (this.offsetLeft === leftoffset);
 								});
 								if (target.length !== 0) {
-									pe.focus(target.first());
+									_pe.focus(target.first());
 								} else {
 									target = $popupLinks.filter(function () {
 										return (this.offsetLeft > leftoffset);
 									});
 									if (target.length !== 0) {
-										pe.focus(target.first());
+										_pe.focus(target.first());
 									} else {
-										pe.focus($popupLinks.first());
+										_pe.focus($popupLinks.first());
 									}
 								}
 								return false;
@@ -232,7 +232,7 @@
 									});
 									if (matches.length !== 0) {
 										if (target.hasClass('bookmark_popup_text') || elmtext.substring(1, 2).toLowerCase() !== keychar) {
-											pe.focus(matches.eq(0));
+											_pe.focus(matches.eq(0));
 										} else {
 											match = matches.length;
 											for (i = 0, len = match; i !== len; i += 1) {
@@ -242,10 +242,10 @@
 												}
 											}
 											if (match < (matches.length - 1)) {
-												pe.focus(matches.eq(match + 1));
+												_pe.focus(matches.eq(match + 1));
 												return false;
 											}
-											pe.focus(matches.eq(0));
+											_pe.focus(matches.eq(0));
 										}
 									}
 									return false;
@@ -261,12 +261,12 @@
 					} else if (e.type === 'open') { // Open the popup menu and put the focus on the first link
 						$popupText.text(opts.hideText + opts.popupText);
 						$popup.attr('aria-hidden', 'false').addClass('show');
-						pe.focus($popup.find('li a').first());
+						_pe.focus($popup.find('li a').first());
 					} else if (e.type === 'close' || e.type === 'closenofocus') { // Close the popup menu
 						$popupText.text(opts.popupText);
 						$popup.attr('aria-hidden', 'true').removeClass('show');
 						if (e.type === 'close') {
-							pe.focus($popupText.first());
+							_pe.focus($popupText.first());
 						}
 					} else if (type === 'mouseenter') { // Mouse hover so ignore click away from link but in popup
 						_pe.fn.share.ignoreFocusoutside = true;
