44954b34769a1e5ac4821bd42247ecb4b26f7aa1
laurent Goderre <laurent.goderre@gmail.com>
2013-04-04 10:04:54 -0400

    Added the missing git add to the post build

--- build/post_build.sh
+++ build/post_build.sh
@@ -80,6 +80,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ] &&  [ "$TRAVIS_REPO_SLUG" == "wet-boew/
 			mkdir demos
 			cp -Rf $HOME/temp_wet-boew/demos/theme-base ./demos/theme-base
 			cp -Rf $HOME/temp_wet-boew/demos/theme-wet-boew ./demos/theme-wet-boew
+			git add -f .
 			git commit -q -m "Travis build $TRAVIS_BUILD_NUMBER pushed to $TRAVIS_BRANCH"
 			git push -fq origin master-base-dist > /dev/null
 		fi
