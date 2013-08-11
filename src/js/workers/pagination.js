104b2f7ef8f326e7802c5168cc5f3f3de9e4aa4e
Nick Schonning <nschonni@gmail.com>
2013-08-11 19:47:28 -0400

    Pagination: Cleanup jshint errors
    - Exclude 3rd Party simplePagination from linting
    - Hoist var declarations
    - Add missing variable declarations and remove unused
    - Compact final pagination call so it didn't used broad class selector
    - Style: rename $divs -> $items

--- src/js/workers/pagination.js
+++ src/js/workers/pagination.js
@@ -5,7 +5,8 @@
 /*
  * Pagination plugin / Plugiciel de pagination
  */
-(function ($) {
+/*global wet_boew_pagination*/
+(function($) {
 	"use strict";
 	var _pe = window.pe || {
 		fn : {}
@@ -14,13 +15,12 @@
 	_pe.fn.pagination = {
 		type : 'plugin',
 		depends : (['simplePagination']),
-		_exec : function (elm) {
-			var $divs = elm.children('div'),
+		_exec : function(elm) {
+			var $items = elm.children('div'),
 				pageCount = 0,
-				previousText,
-				nextText,
 				opts,
-				selector;
+				overrides,
+				itemOverride;
 
 			// Options
 			opts = {
@@ -43,7 +43,7 @@
 
 
 			// Class-based overrides
-			var itemOverride = elm.attr('class').match(/items-per-page-([0-9]*)/);
+			itemOverride = elm.attr('class').match(/items-per-page-([0-9]*)/);
 
 			overrides = {
 				numberOfItemOnPage: itemOverride ? parseInt(itemOverride[1], 10) : opts.numberOfItemOnPage
@@ -52,19 +52,17 @@
 			// Extend the defaults with settings passed through settings.js (wet_boew_lightbox), class-based overrides and the data-wet-boew attribute /
 			$.extend(opts, (typeof wet_boew_pagination !== 'undefined' ? wet_boew_pagination : {}), overrides, _pe.data.getData(elm, 'wet-boew'));
 
-			for (var i = 0, len = $divs.length; i < len; i += opts.numberOfItemOnPage) {
+			for (var i = 0, len = $items.length; i < len; i += opts.numberOfItemOnPage) {
 				pageCount++;
-				$divs.slice(i, i + opts.numberOfItemOnPage).wrapAll("<div class='hide-page' id='page-" + pageCount + "'></div>");
+				$items.slice(i, i + opts.numberOfItemOnPage).wrapAll("<div class='hide-page' id='page-" + pageCount + "'></div>");
 			}
 
 			opts.items = pageCount;
 
 			//Add default pager, should look at options first
-			$(elm).prepend('<div class="page-selector pagination-accent float-right">');
+			elm.prepend('<div class="page-selector pagination-accent float-right">');
 
-			// Buttons for selecting a page
-			$selector = $('.page-selector');
-			$selector.pagination($.extend({}, opts));
+			elm.find('.page-selector').pagination($.extend({}, opts));
 
 			return elm;
 		} // end of exec
