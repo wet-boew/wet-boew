206cfb6a1b0558cd71f911b0fa3b320384b9b0fc
Roch Lambert <roch.lambert@hrsdc-rhdcc.gc.ca>
2013-09-06 10:03:32 -0400

    Update sessiontimeout.js

--- src/js/workers/sessiontimeout.js
+++ src/js/workers/sessiontimeout.js
@@ -9,13 +9,13 @@
 (function ($) {
 	"use strict";
 	var _pe = window.pe || {
-		fn : {}
+		fn: {}
 	};
 
 	/* local reference */
 	_pe.fn.sessiontimeout = {
-		type : 'plugin',
-		_exec : function (elm) {
+		type: 'plugin',
+		_exec: function (elm) {
 			var opts,
 				// An overlay over the screen when showing the dialog message
 				// Added &nbsp; to fix Chrome bug (received from Charlie Lavers - PWGSC)
@@ -33,41 +33,49 @@
 				getExpireTime,
 				alreadyTimeoutMsg = _pe.dic.get('%st-already-timeout-msg'),
 				timeoutMsg = _pe.dic.get('%st-timeout-msg'),
-				lastActivity, 
+				lastActivity,
 				lastCheck = 0;
 
 			// Defaults
 			opts = {
 
-				ajaxLimiter: 200000,		// default period of 2 minutes (ajax calls happen only once during this period)
-				inactivity: 1200000,		// default inactivity period 20 minutes
-				reactionTime: 30000,		// default confirmation period of 30 seconds
-				sessionalive: 1200000,		// default session alive period 20 minutes
-				logouturl: './',			// can't really set a default logout URL
-				refreshOnClick: true,		// refresh session if user clicks on the page
+				ajaxLimiter: 200000, // default period of 2 minutes (ajax calls happen only once during this period)
+				inactivity: 1200000, // default inactivity period 20 minutes
+				reactionTime: 30000, // default confirmation period of 30 seconds
+				sessionalive: 1200000, // default session alive period 20 minutes
+				logouturl: './', // can't really set a default logout URL
+				refreshOnClick: true, // refresh session if user clicks on the page
 				// Ajax call back url function to server to keep the session alive, this has to return true or false from server on success
-				refreshCallbackUrl: './',	// Can't really set a default callbackurl
-				regex:	/^([0-9]+(?:\.[0-9]*)?)\s*(.*s)?$/,
-				powers:	{'ms': 1, 'cs': 10, 'ds': 100, 's': 1000, 'das': 10000, 'hs': 100000, 'ks': 1000000}
+				refreshCallbackUrl: './', // Can't really set a default callbackurl
+				regex: /^([0-9]+(?:\.[0-9]*)?)\s*(.*s)?$/,
+				powers: {
+					'ms': 1,
+					'cs': 10,
+					'ds': 100,
+					's': 1000,
+					'das': 10000,
+					'hs': 100000,
+					'ks': 1000000
+				}
 			};
 
 			// Extend the defaults with settings passed through settings.js (wet_boew_sessiontimeout) and the data-wet-boew attribute
 			$.extend(opts, (typeof wet_boew_sessiontimeout !== 'undefined' ? wet_boew_sessiontimeout : {}), _pe.data.getData(elm, 'wet-boew'));
-		
+
 			//------------------------------------------------------ Main functions
 
 			keep_session = function () {
 				clearTimeout(sessionTimeout);
 				// If the refreshCallbackUrl not present then dont show any error
 				if (opts.refreshCallbackUrl.length > 2) {
-					$.post(opts.refreshCallbackUrl,	function (responseData) {
+					$.post(opts.refreshCallbackUrl, function (responseData) {
 						// if the response data returns anything but "true", we should display that the session has timed out.
 						if (responseData && responseData.replace(/\s/g, '') === 'true') {
 							sessionTimeout = setTimeout(keep_session, timeParse(opts.sessionalive));
 						} else {
 							alert(alreadyTimeoutMsg);
 							redirect();
-						}	
+						}
 					});
 				}
 			};
@@ -96,8 +104,8 @@
 			logout = function () {
 				var start = getCurrentTimeMs();
 
-				// because of short circuit evaluation, this statement 
-				// will show the dialog before evaluating the time, thus the 
+				// because of short circuit evaluation, this statement
+				// will show the dialog before evaluating the time, thus the
 				// getCurrentTimeMs() will return the time after the alert
 				// box is shown.
 				if (displayTimeoutMessage() && getCurrentTimeMs() - start <= opts.reactionTime) {
@@ -108,7 +116,7 @@
 			};
 
 			//--------------------------------------------------- Utility functions
-	
+
 			getCurrentTimeMs = function () {
 				return (new Date()).getTime();
 			};
@@ -118,7 +126,7 @@
 			};
 
 			stay_logged_in = start_liveTimeout;
-		
+
 			// Parsing function for time period
 			timeParse = function (value) {
 				var result, num, mult;
@@ -138,11 +146,11 @@
 
 			getExpireTime = function () {
 				var expire = new Date(getCurrentTimeMs() + opts.reactionTime),
-					hours = expire.getHours(), 
-					minutes = expire.getMinutes(), 
+					hours = expire.getHours(),
+					minutes = expire.getMinutes(),
 					seconds = expire.getSeconds(),
 					timeformat = hours < 12 ? ' AM' : ' PM';
-					
+
 				hours = hours % 12;
 				if (hours === 0) {
 					hours = 12;
@@ -155,11 +163,11 @@
 
 				return hours + ':' + minutes + ':' + seconds + timeformat;
 			};
-		
+
 
 			// Prevent the initial ajax call from happening, instead wait for the inactivity time to pass before this call is made
 			// start_liveTimeout();
-			setTimeout(start_liveTimeout(),timeParse(opts.inactivity));
+			setTimeout(start_liveTimeout(), timeParse(opts.inactivity));
 
 			if (opts.refreshOnClick) {
 				_pe.document.on('click', function (e) {
@@ -168,15 +176,16 @@
 					if (lastActivity >= 1 && (getCurrentTimeMs() - lastCheck) > opts.ajaxLimiter) {
 						var button = e.button;
 
-						if (typeof button === 'undefined' || button === _pe.leftMouseButton) { lastCheck = getCurrentTimeMs(); }
-
 						if (typeof button === 'undefined' || button === _pe.leftMouseButton) { // Ignore middle/right mouse buttons
+							lastCheck = getCurrentTimeMs();
 							start_liveTimeout();
 						}
 
 					} // END OF if (lastActivity >= 1 && ...
 
-					if (typeof e.button === 'undefined' || e.button === _pe.leftMouseButton) { lastActivity = getCurrentTimeMs(); }
+					if (typeof e.button === 'undefined' || e.button === _pe.leftMouseButton) {
+						lastActivity = getCurrentTimeMs();
+					}
 				});
 			}
 
