315e67ad0a8a4be4717559408dec778dc2c47c1d
Pat Heard <pat.heard@gmail.com>
2013-09-12 11:44:32 -0400

    Jaws aria polyfill: jQuery style guide fixes

--- src/polyfills/jawsariafixes/jawsariafixes.js
+++ src/polyfills/jawsariafixes/jawsariafixes.js
@@ -1,6 +1,6 @@
 /*!
  *
- * Web Experience Toolkit (WET) / Boîte à outils de l'expérience Web (BOEW)
+ * Web Experience Toolkit (WET) / BoÃ®te Ã  outils de l'expÃ©rience Web (BOEW)
  * wet-boew.github.io/wet-boew/License-en.html / wet-boew.github.io/wet-boew/Licence-fr.html
  *
  * Version: @wet-boew-build.version@
@@ -10,36 +10,33 @@
  * Jaws WAI-ARIA fixes
  */
 
-(function ($) {
+(function( $ ) {
 	"use strict";
-	var nodes = $('nav, section, article, aside, main').get(),
-		node,
-		headings = $('h1, h2, h3, h4, h5, h6').get(),
-		heading,
-		i,
+	var heading, i, id, node,
+		headings = $( "h1, h2, h3, h4, h5, h6" ).get(),
+		nodes = $( "nav, section, article, aside, main" ).get(),
 		len = nodes.length,
-		id,
-		idPrefix = 'wb-h-aria-';
+		idPrefix = "wb-h-aria-";
 
 	// Use aria-labelledby to fix the Jaws sectioning element/heading element association bug
-	for (i = 0; i !== len; i += 1) {
-		node = nodes[i];
-		heading = (node.firstElementChild || node.firstChild);
-		if (heading.nodeName.match(/h[1-6]/i) !== null) {
+	for ( i = 0; i !== len; i += 1 ) {
+		node = nodes[ i ];
+		heading = ( node.firstElementChild || node.firstChild );
+		if ( heading.nodeName.match( /h[1-6]/i ) !== null ) {
 			id = heading.id;
-			if (id.length === 0) {
+			if ( id.length === 0 ) {
 				id = idPrefix + i;
 				heading.id = id;
 			}
-			node.setAttribute('aria-labelledby', id);
+			node.setAttribute( "aria-labelledby", id );
 		}
 	}
 
 	// Use role="heading" and aria-level to fix the Jaws sectioning element/heading level bug
 	len = headings.length;
-	for (i = 0; i !== len; i += 1) {
-		heading = headings[i];
-		heading.setAttribute('role', 'heading');
-		heading.setAttribute('aria-level', heading.nodeName.substring(1));
+	for ( i = 0; i !== len; i += 1 ) {
+		heading = headings[ i ];
+		heading.setAttribute( "role", "heading" );
+		heading.setAttribute( "aria-level", heading.nodeName.substring( 1 ) );
 	}
-}(jQuery));
+}( jQuery ));
