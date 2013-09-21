b0f5ad44a9423827829b6be94c422de2a635acdc
schindld <dave.schindler@gmail.com>
2013-09-21 12:36:33 -0400

    Added a basic sketch of how the tabbed interface plugin is going to make use of the carousel plugin classes and events.

--- src/plugins/tabbedinterface/tabbedinterface.js
+++ src/plugins/tabbedinterface/tabbedinterface.js
@@ -7,20 +7,47 @@
  */
 /*jshint unused: false */
 /*global console */
-(function( $, win, doc ) {
+(function( $, win, vapour ) {
 	"use strict";
-	var selector = ".wet-boew-tabbedinterface",
+	var selector = ".wet-boew-tabbedinterface, .wb-tabbedinterface",
+		$doc = vapour.doc,
 		init = function() {
-			var pluginElements = $(this);
+			var $widget = $(this),
+				style,
+				controls;
 			win._timer.remove( selector );
-			// your code here...
-			console.log(this);
+			
+			$widget.addClass('wb-carousel');
+			$widget.children('.tabs-panel').children().addClass('item').first().addClass('in');
+			style = $widget.attr('class').match(/tabs-style-(\d+)/);
+			// style is something like ["tabs-style-2", "2", index: 25, input: "wet-boew-tabbedinterface tabs-style-2 cycle-slow animate slide-horz wb-carousel"]
+			if (style && style[1] > 1) {
+				controls = $('<ul class="tabs-controls"><li class="tabs-toggle"><a class="prv" href="javascript:;" role="button">&nbsp;&nbsp;&nbsp;<span class="wb-invisible">Previous</span></a></li><li class="tabs-toggle"><a class="nxt" href="javascript:;" role="button">&nbsp;&nbsp;&nbsp;<span class="wb-invisible">Next</span></a></li><li class="tabs-toggle"><a class="plypause" href="javascript:;" role="button">Play<span class="wb-invisible">  -  Start tab rotation</span></a></li></ul>');
+				$widget.append(controls);
+			}
+			$widget.trigger('carousel.init.wb');
+			//console.log(this);
 		};
 	
 	// Bind the init event of the plugin
-	$(doc).on( "wb.timerpoke", selector, init );
+	$doc.on( "wb.timerpoke", selector, init );
 
 	// Add the timer poke to initialize the plugin
 	win._timer.add( selector );
 
-}( jQuery, window, document ));
+}( jQuery, window, vapour ));
+
+/*
+For reference, vapour is an object:
+{
+	"/": "file:///C:/Users/Dave/Documents/GitHub/schindld/wet-boew/dist/js", 
+	"/assets": "file:///C:/Users/Dave/Documents/GitHub/schindld/wet-boew/dist/js/assets", 
+	"/templates": "file:///C:/Users/Dave/Documents/GitHub/schindld/wet-boew/dist/js/assets/templates", 
+	"/deps": "file:///C:/Users/Dave/Documents/GitHub/schindld/wet-boew/dist/js/deps", 
+	"mode": ".min",
+	"doc": $(document),
+	"getMode": function (){return this.mode},
+	"getPath": function (a){var b;return b=this.hasOwnProperty(a)?this[a]:c},
+	"mode": ".min"
+}
+*/
