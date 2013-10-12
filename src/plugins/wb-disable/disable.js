fe2905618849e63540f0b906e21e893fd82009ff
pjackson28 <pjackson28@hotmail.com>
2013-10-11 23:39:08 -0400

    Fixed wbdisable links and localStorage handling

--- src/plugins/wb-disable/disable.js
+++ src/plugins/wb-disable/disable.js
@@ -15,7 +15,6 @@
 var selector = "wb-tphp",
 	$document = vapour.doc,
 
-
 /*
  * createOffer runs once per plugin element on the page. 
  * @method createOffer
@@ -29,32 +28,37 @@ createOffer = function( elm ) {
 		i18n = window.i18n;
 
 	li.className = "wb-skip";
-	
-	for ( param in pageUrl.params ) { // Rebuild the query string
-			if ( pageUrl.params.hasOwnProperty( param ) && param !== "wbdisable" ) {
-				nQuery += param + "=" + pageUrl.params[ param ] + "&#38;";
-			}
+
+	// Rebuild the query string
+	for ( param in pageUrl.params ) {
+		if ( pageUrl.params.hasOwnProperty( param ) && param !== "wbdisable" ) {
+			nQuery += param + "=" + pageUrl.params[ param ] + "&#38;";
+		}
+	}
+
+	if ( vapour.isDisabled || ( vapour.ie && vapour.ielt7 ) ) {
+		$html.addClass( "no-js wb-disable" );
+		if ( localStorage ) {
+
+			// Store preference for WET plugins and polyfills to be disabled in localStorage
+			localStorage.setItem( "wbdisable", "true");
 		}
 
-		// TODO: Find better way than browser sniffing to single out IE6
-		if ( vapour.wbDisable || ( vapour.ie && vapour.ielt7 ) ) {
-			$html.addClass( "no-js wb-disable" );
-			if ( localStorage ) {
-				// Store preference for WET plugins and polyfills to be disabled in localStorage
-				localStorage.setItem( "wbdisable", "true");
-			}
+		// Append the Standard version link
+		li.innerHTML = "<a class='wb-skip-link' href='" + nQuery + "wbdisable=false'>" + i18n( "%wb-enable" ) + "</a>";
+		
+		// Add link to re-enable WET plugins and polyfills
+		elm.appendChild( li );
+		return true;
+	} else if ( localStorage ) {
 
-			// Append the Standard version link
-			li.innerHTML = "<a class='wb-skip-link' href='" + nQuery + "wbdisable=false'>" + i18n( "%wb-enable" ) + "</a>";
-			elm.appendChild( li ); // Add link to re-enable WET plugins and polyfills
-			return true;
-		} else if ( localStorage ) {
-			localStorage.setItem( "wbdisable", "false" ); // Store preference for WET plugins and polyfills to be enabled in localStorage
-		}
+		// Store preference for WET plugins and polyfills to be enabled in localStorage
+		localStorage.setItem( "wbdisable", "false" );
+	}
 
-		// Append the Basic HTML version link version
-		li.innerHTML = "<a class='wb-skip-link' href='" + nQuery + "wbdisable=true'>" + i18n( "%wb-disable" ) + "</a>";
-		elm.appendChild( li ); // Add link to disable WET plugins and polyfills
+	// Append the Basic HTML version link version
+	li.innerHTML = "<a class='wb-skip-link' href='" + nQuery + "wbdisable=true'>" + i18n( "%wb-disable" ) + "</a>";
+	elm.appendChild( li ); // Add link to disable WET plugins and polyfills
 };
 
 	
@@ -64,7 +68,6 @@ $document.on( "disable.wb", function() {
 	if ( topOfPage ) {
 		createOffer( topOfPage );
 	}
-	
 });
 
 })( jQuery, window, vapour );
