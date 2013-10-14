46964656b33e70b8ec9bf09add9a3a23bb6b55eb
pjackson28 <pjackson28@hotmail.com>
2013-10-13 20:34:58 -0400

    Cleaned up vapour.js formatting

--- src/plugins/wb-disable/disable.js
+++ src/plugins/wb-disable/disable.js
@@ -4,7 +4,6 @@
  * @license wet-boew.github.io/wet-boew/License-en.html / wet-boew.github.io/wet-boew/Licence-fr.html
  * @author @gc
  */
-
 (function( $, window, vapour ) {
 "use strict";
 
@@ -16,60 +15,60 @@
 var selector = vapour.sDisabled,
 	$document = vapour.doc,
 
+	/*
+	 * createOffer runs once per plugin element on the page.
+	 * @method createOffer
+	 */
+	createOffer = function( elm ) {
 
-/*
- * createOffer runs once per plugin element on the page.
- * @method createOffer
- */
-createOffer = function( elm ) {
-	// Let remove ourselves from the queue we only run once
-    window._timer.remove( selector );
+		// Let remove ourselves from the queue we only run once
+		window._timer.remove( selector );
 
-	var li = document.createElement( "li" ),
-		pageUrl = vapour.pageUrlParts,
-		nQuery = "?",
-		param,
-		$html = vapour.html,
-		i18n = window.i18n;
+		var li = document.createElement( "li" ),
+			pageUrl = vapour.pageUrlParts,
+			nQuery = "?",
+			param,
+			$html = vapour.html,
+			i18n = window.i18n;
 
-	li.className = "wb-skip";
+		li.className = "wb-skip";
 
 
-	// Rebuild the query string
-	for ( param in pageUrl.params ) {
-		if ( pageUrl.params.hasOwnProperty( param ) && param !== "wbdisable" ) {
-			nQuery += param + "=" + pageUrl.params[ param ] + "&#38;";
-		}
-	}
-
-	if ( vapour.isDisabled || ( vapour.ie && vapour.ielt7 ) ) {
-		$html.addClass( "no-js wb-disable" );
-		if ( localStorage ) {
-
-			// Store preference for WET plugins and polyfills to be disabled in localStorage
-			localStorage.setItem( "wbdisable", "true");
+		// Rebuild the query string
+		for ( param in pageUrl.params ) {
+			if ( pageUrl.params.hasOwnProperty( param ) && param !== "wbdisable" ) {
+				nQuery += param + "=" + pageUrl.params[ param ] + "&#38;";
+			}
 		}
 
-		// Append the Standard version link
-		li.innerHTML = "<a class='wb-skip-link' href='" + nQuery + "wbdisable=false'>" + i18n( "%wb-enable" ) + "</a>";
+		if ( vapour.isDisabled || ( vapour.ie && vapour.ielt7 ) ) {
+			$html.addClass( "no-js wb-disable" );
+			if ( localStorage ) {
 
-		// Add link to re-enable WET plugins and polyfills
-		elm.appendChild( li );
-		return true;
-	} else if ( localStorage ) {
+				// Store preference for WET plugins and polyfills to be disabled in localStorage
+				localStorage.setItem( "wbdisable", "true");
+			}
 
-		// Store preference for WET plugins and polyfills to be enabled in localStorage
-		localStorage.setItem( "wbdisable", "false" );
-	}
+			// Append the Standard version link
+			li.innerHTML = "<a class='wb-skip-link' href='" + nQuery + "wbdisable=false'>" + i18n( "%wb-enable" ) + "</a>";
 
-	// Append the Basic HTML version link version
-	li.innerHTML = "<a class='wb-skip-link' href='" + nQuery + "wbdisable=true'>" + i18n( "%wb-disable" ) + "</a>";
-	elm.appendChild( li ); // Add link to disable WET plugins and polyfills
-};
+			// Add link to re-enable WET plugins and polyfills
+			elm.appendChild( li );
+			return true;
+		} else if ( localStorage ) {
+
+			// Store preference for WET plugins and polyfills to be enabled in localStorage
+			localStorage.setItem( "wbdisable", "false" );
+		}
+
+		// Append the Basic HTML version link version
+		li.innerHTML = "<a class='wb-skip-link' href='" + nQuery + "wbdisable=true'>" + i18n( "%wb-disable" ) + "</a>";
+		elm.appendChild( li ); // Add link to disable WET plugins and polyfills
+	};
 
 // Bind the events
 $document.on( "timerpoke.wb", selector, function() {
-		createOffer( this );
+	createOffer( this );
 });
 
 // Add the timer poke to initialize the plugin
