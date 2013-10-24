7ac2ae507f01ecff38e3939c1f3a1b722a3969b5
Nick Schonning <nschonni@gmail.com>
2013-10-24 01:50:33 -0400

    data-ajax-*: Collapse event switch
    - Use if/else since they only have 2 paths in each
    - Wrap lines at 80 chars for ease of reading GH diffs
    - Trim trailing spacees

--- src/plugins/data-ajax/data-ajax.js
+++ src/plugins/data-ajax/data-ajax.js
@@ -1,5 +1,6 @@
 /*
- * @title WET-BOEW Data Ajax [data-ajax-after], [data-ajax-append], [data-ajax-before], [data-ajax-prepend] and [data-ajax-replace] 
+ * @title WET-BOEW Data Ajax [data-ajax-after], [data-ajax-append],
+ * [data-ajax-before], [data-ajax-prepend] and [data-ajax-replace]
  * @overview A basic AjaxLoader wrapper that inserts AJAXed in content
  * @license wet-boew.github.io/wet-boew/License-en.html / wet-boew.github.io/wet-boew/Licence-fr.html
  * @author WET Community
@@ -9,27 +10,32 @@
 
 $.ajaxSettings.cache = false;
 
-/* 
- * Variable and function definitions. 
- * These are global to the plugin - meaning that they will be initialized once per page,
- * not once per instance of plugin on the page. So, this is a good place to define
- * variables that are common to all instances of the plugin on a page.
+/*
+ * Variable and function definitions.
+ * These are global to the plugin - meaning that they will be initialized once
+ * per page, not once per instance of plugin on the page. So, this is a good
+ * place to define variables that are common to all instances of the plugin on a
+ * page.
  */
-var selector = "[data-ajax-after], [data-ajax-append], [data-ajax-before], [data-ajax-prepend], [data-ajax-replace]",
-	$document = vapour.doc,
+var $document = vapour.doc,
+	selector = "[data-ajax-after], [data-ajax-append], [data-ajax-before], " +
+		"[data-ajax-prepend], [data-ajax-replace]",
 
 	/*
-	 * Init runs once per plugin element on the page. There may be multiple elements. 
-	 * It will run more than once per plugin if you don't remove the selector from the timer.
+	 * Init runs once per plugin element on the page. There may be multiple
+	 * elements. It will run more than once per plugin if you don't remove the
+	 * selector from the timer.
 	 * @method init
 	 * @param {jQuery DOM element} $elm The plugin element being initialized
-	 * @param {string} $elm The plugin ajaxType The type of AJAX operation, either after, append, before or replace
+	 * @param {string} $elm The plugin ajaxType The type of AJAX operation,
+	 * either after, append, before or replace
 	 */
 	init = function( $elm, ajaxType ) {
 
 		var _url = $elm.data( "ajax-" + ajaxType );
 
-		// All plugins need to remove their reference from the timer in the init sequence unless they have a requirement to be poked every 0.5 seconds
+		// All plugins need to remove their reference from the timer in the init
+		// sequence unless they have a requirement to be poked every 0.5 seconds
 		window._timer.remove( selector );
 
 		$document.trigger({
@@ -50,8 +56,8 @@ $document.on( "timerpoke.wb ajax-fetched.wb", selector, function( event ) {
 			"prepend"
 		],
 		len = ajaxTypes.length,
-		ajaxType, i, content, newEvent;
-		
+		ajaxType, i, content;
+
 	for ( i = 0; i !== len; i += 1 ) {
 		ajaxType = ajaxTypes[ i ];
 		if ( this.getAttribute( "data-ajax-" + ajaxType ) !== null ) {
@@ -59,39 +65,30 @@ $document.on( "timerpoke.wb ajax-fetched.wb", selector, function( event ) {
 		}
 	}
 
-	switch ( eventType ) {
-	case "timerpoke":
+	if ( eventType === "timerpoke" ) {
 		init( $elm, ajaxType );
-		break;
-	case "ajax-fetched":
+	} else {
+
+		// ajax-fetched event
+
 		content = event.pointer.html();
-		newEvent = "ajax-" + ajaxType + "-loaded.wb";
 		$elm.removeAttr( "data-ajax-" + ajaxType );
-	
-		switch ( ajaxType ) {
-		case "before":
-			$elm.before( content );
-			break;
-		case "replace":
+
+		// "replace" is the only event that doesn't map to a jQuery function
+
+		if ( ajaxType === "replace") {
 			$elm.html( content );
-			break;
-		case "after":
-			$elm.after( content );
-			break;
-		case "append":
-			$elm.append( content );
-			break;
-		case "prepend":
-			$elm.prepend( content );
-			break;
+		} else {
+			$elm[ ajaxType ]( content );
 		}
-		$elm.trigger( newEvent );
-		break;
+
+		$elm.trigger( "ajax-" + ajaxType + "-loaded.wb" );
 	}
 
 	/*
-	 * Since we are working with events we want to ensure that we are being passive about our control, 
-	 * so returning true allows for events to always continue
+	 * Since we are working with events we want to ensure that we are being
+	 * passive about our control, so returning true allows for events to always
+	 * continue
 	 */
 	return true;
 } );
