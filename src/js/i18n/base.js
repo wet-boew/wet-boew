c6e92ffc545e940b9eb7a65c5506200b5c7babe3
Pat Heard <pat.heard@gmail.com>
2013-10-25 08:38:59 -0400

    Session: usability improvements to timeout message (#3190)
    1. Simplified session timeout message to make it easier to understand.
    2. Increased default inactivity time to 3 minutes.

--- src/js/i18n/base.js
+++ src/js/i18n/base.js
@@ -146,7 +146,8 @@
 		'%table-mention': '@%table-mention@',
 		'%table-following': '@%table-following@',
 		/* Session timeout */
-		'%st-timeout-msg': '@%st-timeout-msg@',
+		'%st-timeout-msg-bgn': '@%st-timeout-msg-bgn@',
+		'%st-timeout-msg-end': '@%st-timeout-msg-end@',
 		'%st-msgbox-title': '@%st-msgbox-title@',
 		'%st-already-timeout-msg': '@%st-already-timeout-msg@',
 		/* Disable/enable PE */
