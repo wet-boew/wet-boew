bf4d9cbd8458b4b355fcbee1a5a0d963b8ee6573
Nick Schonning <nschonni@gmail.com>
2013-11-01 00:05:17 -0400

    Remove unnecessary window wrapping for Modernizr

--- src/plugins/prettify/prettify.js
+++ src/plugins/prettify/prettify.js
@@ -33,8 +33,8 @@
 (function( $, window, vapour ) {
 "use strict";
 
-/* 
- * Variable and function definitions. 
+/*
+ * Variable and function definitions.
  * These are global to the plugin - meaning that they will be initialized once per page,
  * not once per instance of plugin on the page. So, this is a good place to define
  * variables that are common to all instances of the plugin on a page.
@@ -52,7 +52,7 @@ var selector = ".wb-prettify",
 	},
 
 	/*
-	 * Init runs once per plugin element on the page. There may be multiple elements. 
+	 * Init runs once per plugin element on the page. There may be multiple elements.
 	 * It will run more than once per plugin if you don't remove the selector from the timer.
 	 * @method init
 	 * @param {jQuery DOM element} $elm The plugin element being initialized
@@ -94,7 +94,7 @@ var selector = ".wb-prettify",
 		}
 
 		// Load the required dependencies and prettify the code once finished
-		window.Modernizr.load({
+		Modernizr.load({
 			load: deps,
 			complete: function() {
 				$document.trigger( "prettyprint.wb-prettify" );
--- src/plugins/twitter/twitter.js
+++ src/plugins/twitter/twitter.js
@@ -7,8 +7,8 @@
 (function( $, window, vapour ) {
 "use strict";
 
-/* 
- * Variable and function definitions. 
+/*
+ * Variable and function definitions.
  * These are global to the plugin - meaning that they will be initialized once per page,
  * not once per instance of plugin on the page. So, this is a good place to define
  * variables that are common to all instances of the plugin on a page.
@@ -17,7 +17,7 @@ var selector = ".wb-twitter",
 	$document = vapour.doc,
 
 	/*
-	 * Init runs once per plugin element on the page. There may be multiple elements. 
+	 * Init runs once per plugin element on the page. There may be multiple elements.
 	 * It will run more than once per plugin if you don't remove the selector from the timer.
 	 * @method init
 	 * @param {jQuery DOM element} $elm The plugin element being initialized
@@ -29,7 +29,7 @@ var selector = ".wb-twitter",
 		// All plugins need to remove their reference from the timer in the init sequence unless they have a requirement to be poked every 0.5 seconds
 		window._timer.remove( selector );
 
-		window.Modernizr.load( {
+		Modernizr.load( {
 			load: ( protocol.indexOf( "http" ) === -1 ? "http:" : protocol ) + "//platform.twitter.com/widgets.js"
 		});
 	};
@@ -38,7 +38,7 @@ $document.on( "timerpoke.wb", selector, function() {
 	init();
 
 	/*
-	 * Since we are working with events we want to ensure that we are being passive about our control, 
+	 * Since we are working with events we want to ensure that we are being passive about our control,
 	 * so returning true allows for events to always continue
 	 */
 	return true;
