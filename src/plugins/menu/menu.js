becc44a67721271b43ecdcc136e51bb26bb0661b
Paul Jackson <pjackson28@hotmail.com>
2013-11-09 23:22:12 -0500

    Menu: Added navigating submenus by the first letter

--- src/plugins/menu/menu.js
+++ src/plugins/menu/menu.js
@@ -22,263 +22,281 @@ var selector = ".wb-menu",
 	menuCount = 0,
 	globalTimeout = {},
 
-/*
- * Lets leverage JS assigment deconstruction to reduce the code output
- * @method expand
- * @param {DOM element} element The plugin element
- * @param {boolean} scopeitems ***Description needed***
- */
-expand = function( element, scopeitems ) {
-	var $elm = $( element ),
-		_elm = $elm.hasClass( "wb-menu" ) ? $elm.data() : $elm.parents( ".wb-menu" )
-			.first()
-			.data(),
-		_items = scopeitems ? _elm.items.has( element ) : _elm.items;
-	return [ _elm.self, _elm.menu, _items, $elm ];
-},
+	/*
+	 * Lets leverage JS assigment deconstruction to reduce the code output
+	 * @method expand
+	 * @param {DOM element} element The plugin element
+	 * @param {boolean} scopeitems ***Description needed***
+	 */
+	expand = function( element, scopeitems ) {
+		var $elm = $( element ),
+			elm = $elm.hasClass( "wb-menu" ) ? $elm.data() : $elm.parents( ".wb-menu" )
+				.first()
+				.data(),
+			items = scopeitems ? elm.items.has( element ) : elm.items;
+		return [ elm.self, elm.menu, items, $elm ];
+	},
 
-/*
- * Lets set some aria states and attributes
- * @method onInit
- * @param {jQuery DOM element} element The plugin element
- */
-onInit = function( $elm ) {
+	/*
+	 * Lets set some aria states and attributes
+	 * @method onInit
+	 * @param {jQuery DOM element} element The plugin element
+	 */
+	onInit = function( $elm ) {
 
-	// All plugins need to remove their reference from the timer in the init sequence unless they have a requirement to be poked every 0.5 seconds
-	window._timer.remove( selector );
+		// All plugins need to remove their reference from the timer in the init
+		// sequence unless they have a requirement to be poked every 0.5 seconds
+		window._timer.remove( selector );
 
-	// Ensure the container has an id attribute
-	if ( !$elm.attr( "id" ) ) {
-		$elm.attr( "id", "wb-menu-" + menuCount );
-	}
-	menuCount += 1;
+		// Ensure the container has an id attribute
+		if ( !$elm.attr( "id" ) ) {
+			$elm.attr( "id", "wb-menu-" + menuCount );
+		}
+		menuCount += 1;
 
-	// Lets test to see if we have any
-	if ( $elm.data( "ajax-fetch" ) ) {
-		$document.trigger({
-			type: "ajax-fetch.wb",
-			element: $elm,
-			fetch: $elm.data( "ajax-fetch" )
-		});
-	} else {
-		$elm.trigger( "loaded.wb-menu" );
-	}
-},
+		// Lets test to see if we have any
+		if ( $elm.data( "ajax-fetch" ) ) {
+			$document.trigger({
+				type: "ajax-fetch.wb",
+				element: $elm,
+				fetch: $elm.data( "ajax-fetch" )
+			});
+		} else {
+			$elm.trigger( "loaded.wb-menu" );
+		}
+	},
 
-/*
- * Lets set some aria states and attributes
- * @method drizzleAria
- * @param {jQuery DOM elements} collection of elements
- */
-drizzleAria = function( $elements ){
-	var length = $elements.length,
-		elm, subMenu, i;
+	/*
+	 * Lets set some aria states and attributes
+	 * @method drizzleAria
+	 * @param {jQuery DOM elements} collection of elements
+	 */
+	drizzleAria = function( $elements ) {
+		var length = $elements.length,
+			$elm, $subMenu, i;
 
-	// Lets tweak for aria
-	for ( i = 0; i <= length; i++ ) {
-		elm = $elements.eq( i );
-		subMenu = elm.siblings( ".sm" );
+		// Lets tweak for aria
+		for ( i = 0; i !== length; i += 1 ) {
+			$elm = $elements.eq( i );
+			$subMenu = $elm.siblings( ".sm" );
 
-		elm.attr({
-			"aria-posinset": ( i + 1 ),
-			"aria-setsize": length,
-			"role": "menuitem"
-		});
-
-		// if there is a submenu lets put in the aria for it
-		if ( subMenu.length > 0 ) {
-
-			elm.attr({
-				"aria-haspopup": "true"
+			$elm.attr({
+				"aria-posinset": ( i + 1 ),
+				"aria-setsize": length,
+				"role": "menuitem"
 			});
 
-			subMenu.attr({
-				"aria-expanded": "false",
-				"aria-hidden": "true"
-			});
+			// if there is a submenu lets put in the aria for it
+			if ( $subMenu.length !== 0 ) {
 
-			// recurse into submenu
-			drizzleAria( subMenu.find( ":discoverable" ) );
+				$elm.attr( "aria-haspopup", "true" );
+
+				$subMenu.attr({
+					"aria-expanded": "false",
+					"aria-hidden": "true"
+				});
+
+				// recurse into submenu
+				drizzleAria( $subMenu.find( ":discoverable" ) );
+			}
 		}
-	}
-},
+	},
 
-/*
- * @method onAjaxLoaded
- * @param {jQuery DOM elements} element The plugin element
- */
-onAjaxLoaded = function( $elm, $ajaxed ) {
+	/*
+	 * @method onAjaxLoaded
+	 * @param {jQuery DOM elements} element The plugin element
+	 */
+	onAjaxLoaded = function( $elm, $ajaxed ) {
+		var $menu = $ajaxed.find( "[role='menubar'] .item" ),
+			$wbsec = $( "#wb-sec" ),
+			$panel;
 
-	//Some hooks for post transformation
-	// - @data-post-remove : removes the space delimited class for the element. This is more a feature to fight the FOUC
-	var $menu = $ajaxed.find( "[role='menubar'] .item" ),
-		$panel,
-		$wbsec = $( "#wb-sec" );
-
-	// lets see if we need to add a dynamic navigation section ( secondary nav )
-	if ( $wbsec.length !== 0 ) {
-		$panel = $ajaxed.find( ".pnl-strt" );
-		$panel.before( "<section id='dyn-nvgtn' class='" + $panel.siblings( ".wb-nav" ).eq( 0 ).attr( "class" ) + "'>" + $wbsec.html() + "</section>" );
-	}
-
-	$ajaxed.find( ":discoverable" )
-		.attr( "tabindex", "-1" );
-
-	$menu.eq( 0 ).attr( "tabindex", "0" );
-	$menu.filter( "[href^=#]" )
-		.append( "<span class='expicon'></span>" );
-
-	drizzleAria( $menu );
-
-	// Now lets replace the html since we were working off canvas for performance
-	if ( $elm.has( "[data-post-remove]" ) ) {
-		$elm.removeClass( $elm.data( "post-remove" ) )
-			.removeAttr( "data-post-remove" );
-	}
-
-	// Replace elements
-	$elm.html( $ajaxed.html() );
-
-	// Recalibrate context
-	$elm.data({
-		self: $elm,
-		menu: $elm.find( "[role=menubar] .item" ),
-		items: $elm.find( ".sm" )
-	});
-
-},
-
-
-/*
- * @method onSelect
- * @param {jQuery event} event The current event
- */
-onSelect = function( event ) {
-
-	event.goto.trigger( "focus.wb" );
-	if ( event.special ) {
-		onReset( event.goto.parents( selector ), true, true );
-	}
-
-},
-
-/*
- * @method onIncrement
- * @param {jQuery DOM element} element The plugin element
- * @param {jQuery event} event The current event
- */
-onIncrement = function( $elm, event ) {
-	var $links = event.cnode,
-		$next = event.current + event.increment,
-		$index = $next;
-
-	if ( $next >= $links.length ) {
-		$index = 0;
-	} else if ( $next < 0 ) {
-		$index = $links.length - 1;
-	}
-
-	$elm.trigger({
-		type: "select.wb-menu",
-		goto: $links.eq( $index )
-	});
-},
-
-/*
- * @method onReset
- * @param {jQuery DOM element} $elm The plugin element
- * @param {boolean} cancelDelay Whether or not to delay the closing of the menus (false by default)
- * @param {boolean} keeptActive Whether or not to leave the active class alone (false by default)
- */
-onReset = function( $elm, cancelDelay, keepActive ) {
-	var id = $elm.attr( "id" ),
-		$openActive = $elm.find( ".open, .active" );
-
-	// Clear any timeouts for open/closing menus
-	clearTimeout( globalTimeout[ id ] );
-
-	if ( cancelDelay ) {
-		$openActive.removeClass( "open sm-open" );
-		if ( !keepActive ) {
-			$openActive.removeClass( "active" );
+		// lets see if we need to add a dynamic navigation section ( secondary nav )
+		if ( $wbsec.length !== 0 ) {
+			$panel = $ajaxed.find( ".pnl-strt" );
+			$panel.before( "<section id='dyn-nvgtn' class='" +
+				$panel.siblings( ".wb-nav" ).eq( 0 ).attr( "class" ) +
+				"'>" + $wbsec.html() + "</section>" );
 		}
-	} else {
 
-		// Delay the closing of the menus
-		globalTimeout[ id ] = setTimeout( function() {
-				$openActive.removeClass( "open sm-open active" );
-		}, hoverDelay );
-	}
-},
+		$ajaxed.find( ":discoverable" )
+			.attr( "tabindex", "-1" );
 
-/*
- * @method onDisplay
- * @param {jQuery DOM element} $elm The plugin element
- * @param {jQuery event} event The current event
+		$menu.eq( 0 ).attr( "tabindex", "0" );
+		$menu.filter( "[href^=#]" )
+			.append( "<span class='expicon'></span>" );
 
- */
-onDisplay = function( $elm, event ) {
-	var menuItem = event.ident,
-		menuLink = menuItem.children( "a" );
+		drizzleAria( $menu );
 
-	// Lets reset the menus with no delay to ensure no overlap
-	$elm.find( ".open, .active" ).removeClass( "open sm-open active" );
+		// Now lets replace the html since we were working off canvas for performance
+		if ( $elm.has( "[data-post-remove]" ) ) {
+			$elm.removeClass( $elm.data( "post-remove" ) )
+				.removeAttr( "data-post-remove" );
+		}
 
-	// Ignore if doesn't have a submenu
-	if ( menuLink.attr( "aria-haspopup" ) === "true" ) {
+		// Replace elements
+		$elm.html( $ajaxed.html() );
 
-		// Add the open state classes
-		menuItem
-			.addClass( "active sm-open" )
-			.find( ".sm" )
-			.addClass( "open" );
-	}
-},
-
-/*
- * @method onHoverFocus
- * @param {jQuery event} event The current event
- */
-onHoverFocus = function( event ) {
-	var ref = expand( event.target ),
-		$container = ref[ 0 ],
-		$elm = ref[ 3 ];
-
-	if ( $container ) {
-
-		// Clear the any timeouts for open/closing menus
-		clearTimeout( globalTimeout[ $container.attr( "id" ) ] );
-
-		$container.trigger({
-			type: "display.wb-menu",
-			ident: $elm.parent(),
-			cancelDelay: event.type === "focusin"
+		// Recalibrate context
+		$elm.data({
+			self: $elm,
+			menu: $elm.find( "[role=menubar] .item" ),
+			items: $elm.find( ".sm" )
 		});
-	}
-},
 
-/*
- * Causes clicks on panel menu items to open and close submenus (except for mouse)
- * @method onPanelClick
- * @param {jQuery event} event The current event
- */
-onPanelClick = function( event ) {
-	var which = event.which,
-		$this;
+	},
 
-	if ( which === 1 ) {
-		event.preventDefault();
-	} else if ( !which ) {
-		event.preventDefault();
-		$this = $( this );
-		if ( $( "#wb-sm" ).find( ".nav-close" ).is( ":visible" ) ) {
-			$this.trigger( "focusin" );
+
+	/*
+	 * @method onSelect
+	 * @param {jQuery event} event The current event
+	 */
+	onSelect = function( event ) {
+
+		event.goto.trigger( "focus.wb" );
+		if ( event.special ) {
+			onReset( event.goto.parents( selector ), true, true );
+		}
+
+	},
+
+	/*
+	 * @method onIncrement
+	 * @param {jQuery DOM element} element The plugin element
+	 * @param {jQuery event} event The current event
+	 */
+	onIncrement = function( $elm, event ) {
+		var $links = event.cnode,
+			$next = event.current + event.increment,
+			$index = $next >= $links.length ? 0 : $next < 0 ? $links.length - 1 : $next;
+
+		$elm.trigger({
+			type: "select.wb-menu",
+			goto: $links.eq( $index )
+		});
+	},
+
+	/*
+	 * @method onReset
+	 * @param {jQuery DOM element} $elm The plugin element
+	 * @param {boolean} cancelDelay Whether or not to delay the closing of the menus (false by default)
+	 * @param {boolean} keeptActive Whether or not to leave the active class alone (false by default)
+	 */
+	onReset = function( $elm, cancelDelay, keepActive ) {
+		var id = $elm.attr( "id" ),
+			$openActive = $elm.find( ".open, .active" );
+
+		// Clear any timeouts for open/closing menus
+		clearTimeout( globalTimeout[ id ] );
+
+		if ( cancelDelay ) {
+			$openActive.removeClass( "open sm-open" );
+			if ( !keepActive ) {
+				$openActive.removeClass( "active" );
+			}
+		} else {
+
+			// Delay the closing of the menus
+			globalTimeout[ id ] = setTimeout( function() {
+					$openActive.removeClass( "open sm-open active" );
+			}, hoverDelay );
+		}
+	},
+
+	/*
+	 * @method onDisplay
+	 * @param {jQuery DOM element} $elm The plugin element
+	 * @param {jQuery event} event The current event
+
+	 */
+	onDisplay = function( $elm, event ) {
+		var menuItem = event.ident,
+			menuLink = menuItem.children( "a" );
+
+		// Lets reset the menus with no delay to ensure no overlap
+		$elm.find( ".open, .active" ).removeClass( "open sm-open active" );
+
+		// Ignore if doesn't have a submenu
+		if ( menuLink.attr( "aria-haspopup" ) === "true" ) {
+
+			// Add the open state classes
+			menuItem
+				.addClass( "active sm-open" )
+				.find( ".sm" )
+				.addClass( "open" );
+		}
+	},
+
+	/*
+	 * @method onHoverFocus
+	 * @param {jQuery event} event The current event
+	 */
+	onHoverFocus = function( event ) {
+		var ref = expand( event.target ),
+			$container = ref[ 0 ],
+			$elm = ref[ 3 ];
+
+		if ( $container ) {
+
+			// Clear any timeouts for open/closing menus
+			clearTimeout( globalTimeout[ $container.attr( "id" ) ] );
+
+			$container.trigger({
+				type: "display.wb-menu",
+				ident: $elm.parent(),
+				cancelDelay: event.type === "focusin"
+			});
+		}
+	},
+
+	/*
+	 * Causes clicks on panel menu items to open and close submenus (except for mouse)
+	 * @method onPanelClick
+	 * @param {jQuery event} event The current event
+	 */
+	onPanelClick = function( event ) {
+		var which = event.which,
+			$this;
+
+		if ( which === 1 ) {
+			event.preventDefault();
 		} else if ( !which ) {
 			event.preventDefault();
-			onReset( $this, true );
+			$this = $( this );
+			if ( $( "#wb-sm" ).find( ".nav-close" ).is( ":visible" ) ) {
+				$this.trigger( "focusin" );
+			} else if ( !which ) {
+				event.preventDefault();
+				onReset( $this, true );
+			}
 		}
-	}
-};
+	},
+
+	/*
+	 * Searches for the next link that has link text starting with a specific letter
+	 * @method selectByLetter
+	 * @param {integer} charCode The charCode of the letter to search for
+	 * @param {DOM elements} links Collection of links to search
+	 * @param {jQuery DOM element} $container Plugin element
+	 */
+	selectByLetter = function( charCode, links, $container ) {
+		var len = links.length,
+			keyChar = String.fromCharCode( charCode ),
+			link, i;
+
+		for ( i = 0; i !== len; i += 1 ) {
+			link = links[ i ];
+			if ( link.innerHTML.charAt( 0 ) === keyChar ) {
+				$container.trigger({
+					type: "select.wb-menu",
+					goto: $( link )
+				});
+				return true;
+			}
+		}
+
+		return false;
+	};
 
 // Bind the events of the plugin
 $document.on( "timerpoke.wb mouseleave select.wb-menu ajax-fetched.wb increment.wb-menu display.wb-menu", selector, function( event ) {
@@ -409,6 +427,17 @@ $document.on( "keydown", selector + " .item", function( event ) {
 			current: $index
 		});
 		break;
+	default:
+
+		// Letters only
+		if ( which > 64 && which < 91 ) {
+			event.preventDefault();
+			selectByLetter(
+				which,
+				$elm.parent().find( "ul a" ).get(),
+				$container
+			);
+		}
 	}
 });
 
@@ -425,7 +454,7 @@ $document.on( "keydown", selector + " [role=menu]", function( event ) {
 		$elm = ref[ 3 ],
 		$links = $items.find( ":focusable" ),
 		$index = $links.index( $elm[ 0 ] ),
-		$goto;
+		$goto, $parent, result;
 
 	switch ( which ) {
 
@@ -457,6 +486,30 @@ $document.on( "keydown", selector + " [role=menu]", function( event ) {
 	case 9:
 		onReset( $container, true );
 		break;
+
+	default:
+
+		// Letters only
+		if ( which > 64 && which < 91 ) {
+			event.preventDefault();
+			$parent = $elm.parent();
+
+			// Try to find a match in the next siblings
+			result = selectByLetter(
+				which,
+				$parent.nextAll().find( "a" ).get(),
+				$container
+			);
+
+			// If couldn't find a match, try the previous siblings
+			if ( !result ) {
+				result = selectByLetter(
+					which,
+					$parent.prevAll().find( "a" ).get(),
+					$container
+				);
+			}
+		}
 	}
 });
 
