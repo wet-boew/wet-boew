84dd9993fc4dbb3422d3a9035f5b8624c0c3dc98
Paul Jackson <pjackson28@hotmail.com>
2013-12-16 10:34:21 -0500

    Menu: First mobile panel menu with wb-navcurr class is opened by default.

--- src/plugins/menu/menu.js
+++ src/plugins/menu/menu.js
@@ -158,7 +158,7 @@ var pluginName = "wb-menu",
 			panelClose = "</div>",
 			allProperties = [],
 			sectionHtml, $onlypnl, $panel, sections, section,
-			properties, originalHtml, i, j, len, len2;
+			properties, originalHtml, i, j, len, len2, $navCurr;
 
 		/*
 		 * Build the mobile panel
@@ -328,6 +328,13 @@ var pluginName = "wb-menu",
 		setTimeout(function() {
 			$elm.trigger( navCurrentEvent, breadcrumb );
 			$panel.trigger( navCurrentEvent, breadcrumb );
+
+			// Open up the first menu with wb-navcurr
+			$navCurr = $panel.find( ".wb-navcurr" ).first();
+			if ( !$navCurr.hasClass( ".tgl-tab" ) ) {
+				$navCurr = $navCurr.parents( "details" ).children( "summary" );
+			}
+			$navCurr.trigger( "click" );
 		}, 1 );
 	},
 
