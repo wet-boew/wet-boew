65c64bc9528d875b346c357105788aa3e7917f82
Paul Jackson <Paul.Jackson@tbs-sct.gc.ca>
2014-01-07 11:52:01 -0500

    Menu panel: Fixed force opening of wb-navcurr sub-menu in Chrome (fixes #4230)

--- src/plugins/menu/menu.js
+++ src/plugins/menu/menu.js
@@ -341,7 +341,8 @@ var pluginName = "wb-menu",
 				if ( $menuItem.attr( "aria-haspopup" ) === "true" ) {
 					$menuItem
 						.trigger( "click" )
-						.prop( "open", "open" );
+						.parent()
+							.prop( "open", "open" );
 				}
 			}
 		}, 1 );
