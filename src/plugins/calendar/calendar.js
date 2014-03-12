4f7024c9ea34c6f6cd1cfe15c20dd884d8c19471
Stephane Berube <stephane.berube@ic.gc.ca>
2014-03-12 11:11:12 -0400

    Calendar: Ensure JAWS sees space between day and year

--- src/plugins/calendar/calendar.js
+++ src/plugins/calendar/calendar.js
@@ -409,7 +409,7 @@ var $document = wb.doc,
 					cells += "<td id='" + id + "' class='" + ( isCurrentDate ? "cal-currday " : "" ) + className + "'><div><time datetime='" + year + "-" +
 						( month < 9 ? "0" : "" ) + ( month + 1 ) + "-" + ( dayCount < 10 ? "0" : "" ) + dayCount + "'><span class='wb-inv'>" + textWeekDayNames[ day ] +
 						( frenchLang ? ( " </span>" + dayCount + "<span class='wb-inv'> " + textMonthNames[ month ].toLowerCase() + " " ) :
-						( " " + textMonthNames[ month ] + " </span>" + dayCount + "<span class='wb-inv'> " ) ) + year +
+						( " " + textMonthNames[ month ] + " </span>" + dayCount + "<span class='wb-inv'>&nbsp;" ) ) + year +
 						( isCurrentDate ? textCurrentDay : "" ) + "</span></time></div></td>";
 
 					if ( dayCount > lastDay ) {
