wet-boew/wet-boew

commit 329961e783a8545d1904e8009938089cb6799904

Fixed mutlimedia in tabs by resolving event conflicts Fix #4571

src/plugins/multimedia/multimedia.js
@@ -13,7 +13,7 @@ var pluginName = "wb-mltmd",
 	selector = "." + pluginName,
 	initedClass = pluginName + "-inited",
 	initEvent = "wb-init" + selector,
-	seed = 0,
+	uniqueCount = 0,
 	templatetriggered = false,
 	i18n, i18nText,
 	captionsLoadedEvent = "ccloaded" + selector,
@@ -35,7 +35,8 @@ var pluginName = "wb-mltmd",
 	 * @param {jQuery Event} event Event that triggered this handler
 	 */
 	init = function( event ) {
-		var eventTarget = event.target;
+		var eventTarget = event.target,
+			elmId = eventTarget.id;
 
 		// Filter out any events triggered by descendants
 		// and only initialize the element once
@@ -63,11 +64,18 @@ var pluginName = "wb-mltmd",
 				};
 			}
 
+			// Ensure there is an id on the element
+			if ( !elmId ) {
+				elmId = "wb-mm-" + uniqueCount;
+				eventTarget.id = elmId;
+				uniqueCount += 1;
+			}
+
 			if ( !templatetriggered ) {
 				templatetriggered = true;
 				$document.trigger({
 					type: "ajax-fetch.wb",
-					element: $( selector ),
+					element: "#" + elmId,
 					fetch: wb.getPath( "/assets" ) + "/mediacontrols.html"
 				});
 			}
@@ -521,7 +529,7 @@ $document.on( initializedEvent, selector, function() {
 	var $this = $( this ),
 		$media = $this.children( "audio, video" ).eq( 0 ),
 		captions = $media.children( "track[kind='captions']" ).attr( "src" ) || undef,
-		id = $this.attr( "id" ) || "wb-mm-" + ( seed++ ),
+		id = $this.attr( "id" ),
 		mId = $media.attr( "id" ) || id + "-md",
 		type = $media.is( "video" ) ? "video" : "audio",
 		width = type === "video" ? $media.attr( "width" ) || $media.width() : 0,
@@ -537,10 +545,6 @@ $document.on( initializedEvent, selector, function() {
 		}, i18nText),
 		media = $media.get( 0 ),
 		url;
-
-	if ( !$this.attr( "id" ) ) {
-		$this.attr( "id", id );
-	}
 
 	if ( $media.attr( "id" ) === undef ) {
 		$media.attr( "id", mId );

commit 8826ede05659a6d22631fb03c8a6c2f0c987c653

Fixed a bug where only the first player would be enhanced. Bug introduced by 329961e783a8545d1904e8009938089cb6799904 and reported as #4897

src/plugins/multimedia/multimedia.js
@@ -75,7 +75,7 @@ var pluginName = "wb-mltmd",
 				templatetriggered = true;
 				$document.trigger({
 					type: "ajax-fetch.wb",
-					element: "#" + elmId,
+					element: selector,
 					fetch: wb.getPath( "/assets" ) + "/mediacontrols.html"
 				});
 			}