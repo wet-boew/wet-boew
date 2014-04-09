wet-boew/wet-boew

commit c7f47a2afdaa877fa6fbce5b660f7860685a3bbf

Hide cc button for audio player Also changed the code to use CSS instead of DOM changes to modify the player. Fix #5176

src/plugins/multimedia/multimedia.js
@@ -553,6 +553,8 @@ $document.on( initializedEvent, selector, function() {
 		$media.attr( "id", mId );
 	}
 
+	$this.addClass( type );
+
 	$this.data( "properties", data );
 
 	if ( $media.find( "[type='video/youtube']" ).length > 0 ) {
@@ -715,9 +717,6 @@ $document.on( renderUIEvent, selector, function( event, type ) {
 
 	$media.after( tmpl( $this.data( "template" ), data ) );
 	$overlay = $media.next().find( ".wb-mm-ovrly" ).after( $media );
-	if ( type !== "video" ) {
-		$overlay.remove();
-	}
 
 	$player = $( "#" + data.mId );
 	data.player = $player.is( "object" ) ? $player.children( ":first-child" ) : $player;

src/plugins/multimedia/multimedia.scss
@@ -58,6 +58,20 @@
 		}
 	}
 
+	&.audio {
+		.wb-mm-ovrly {
+			@extend %multimedia-display-none;
+		}
+
+		.btn.cc {
+			@extend %multimedia-display-none;
+		}
+
+		.lastpnl {
+			width: 3em;
+		}
+	}
+
 	.display {
 		position: relative;
 		background: #000;

commit 467b797262b5f8dd61971d6f18d02fac5a3c3e2e

Fixed the Closed Caption button not showing on the first video. Bug introduced by c7f47a2afdaa877fa6fbce5b660f7860685a3bbf

src/plugins/multimedia/multimedia.js
@@ -534,7 +534,7 @@ $document.on( initializedEvent, selector, function() {
 		captions = $media.children( "track[kind='captions']" ).attr( "src" ) || undef,
 		id = $this.attr( "id" ),
 		mId = $media.attr( "id" ) || id + "-md",
-		type = $media.is( "video" ) ? "video" : "audio",
+		type = $media.is( "audio" ) ? "audio" : "video",
 		width = type === "video" ? $media.attr( "width" ) || $media.width() : 0,
 		height = type === "video" ? $media.attr( "height" ) || $media.height() : 0,
 		data = $.extend({