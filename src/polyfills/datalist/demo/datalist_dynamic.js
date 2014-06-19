8e419f83667a8ae3b99cf27bf83a6b717979a9d1
Laurent Goderre <laurent.goderre@gmail.com>
2014-06-19 13:12:30 -0400

    Added an example of dynamic datalist Addresses #4627

---
+++ src/polyfills/datalist/demo/datalist_dynamic.js
@@ -0,0 +1,41 @@
+/*
+ * Web Experience Toolkit (WET) / Boîte à outils de l'expérience Web (BOEW)
+ * wet-boew.github.io/wet-boew/License-en.html / wet-boew.github.io/wet-boew/Licence-fr.html
+ */
+(function( $, document, wb ) {
+"use strict";
+
+var $document = wb.doc,
+	pluginSelector = "#plugin",
+	dataList = $( "#issues" );
+
+$document.on( "change", pluginSelector, function( event ) {
+	var pluginName = event.target.value;
+
+	$document.trigger({
+		type: "ajax-fetch.wb",
+		element: this,
+		fetch: encodeURI("https://api.github.com/repos/wet-boew/wet-boew/issues?labels=Plugin: " + pluginName)
+	});
+});
+
+$document.on( "ajax-fetched.wb", pluginSelector, function( event ) {
+	var issues = JSON.parse(event.response),
+		lenIssues = issues.length,
+		indIssue, issue;
+
+	dataList.empty();
+
+	dataList.append( "<!--[if lte IE 9]><select><![endif]-->" );
+
+	for ( indIssue = 0; indIssue < lenIssues; indIssue++ ) {
+		issue = issues[ indIssue ];
+		window.console.log( issue );
+		dataList.append( "<option value=\"" + issue.id + "\">" + issue.title + "</option>" );
+	}
+
+	dataList.append( "<!--[if lte IE 9]></select><![endif]-->" );
+	dataList.trigger( "wb-update.wb-datalist" );
+});
+
+})( jQuery, document, wb );
