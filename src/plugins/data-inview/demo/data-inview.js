0b4367cb60f4357a89f4396967d49c3ea91e3fef
Paul Jackson <Paul.Jackson@tbs-sct.gc.ca>
2014-07-18 12:18:42 -0400

    Data InView: Added events to indicate the view state has changed

---
+++ src/plugins/data-inview/demo/data-inview.js
@@ -0,0 +1,14 @@
+/**
+ * @title WET-BOEW Data InView example
+ * @overview Handles the Data InView view state change events and updates the status on the page.
+ * @license wet-boew.github.io/wet-boew/License-en.html / wet-boew.github.io/wet-boew/Licence-fr.html
+ * @author WET Community
+ */
+(function( $, wb ) {
+"use strict";
+
+wb.doc.on( "all.wb-inview partial.wb-inview none.wb-inview", function( event) {
+	$( event.target ).find( ".view-state-status" ).html( event.type );
+});
+
+})( jQuery, wb );
