b90566c7d4970235f771f3d1c3b3c1878c88a688
Etienne Levesque Guitard <etiennelg@gmail.com>
2014-08-10 13:42:37 -0400

    Cleaned up unused files and renamed things

---
+++ src/plugins/dismiss/dismiss.js
@@ -0,0 +1,12 @@
+/**
+ * @title WET-BOEW Dismiss plugin
+ * @overview A dismissible notice plugin for WET
+ * @license wet-boew.github.io/wet-boew/License-en.html / wet-boew.github.io/wet-boew/Licence-fr.html
+ * @author WET community
+ */
+(function( $, window, document, wb ) {
+"use strict";
+
+
+
+})( jQuery, window, document, wb );
