d4434b78f6005ebbc5455de796592c3f9aea6f05
Etienne Levesque Guitard <etiennelg@gmail.com>
2014-08-10 16:42:46 -0400

    Some tweaks to dismiss code for initial tests

--- src/plugins/dismiss/dismiss.js
+++ src/plugins/dismiss/dismiss.js
@@ -4,9 +4,9 @@
  * @license wet-boew.github.io/wet-boew/License-en.html / wet-boew.github.io/wet-boew/Licence-fr.html
  * @author WET community
  */
+ /*
 (function( $, window, document, wb ) {
 "use strict";
 
-
-
 })( jQuery, window, document, wb );
+*/
