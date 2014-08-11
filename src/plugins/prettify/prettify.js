71bb86926c0a2b35c7cc8a186f0ea2d6971a326c
Laurent Goderre <laurent.goderre@gmail.com>
2014-08-11 12:37:55 -0400

    Prettify: Trigger the ready event when prettify has finished processing

--- src/plugins/prettify/prettify.js
+++ src/plugins/prettify/prettify.js
@@ -107,6 +107,12 @@ var componentName = "wb-prettify",
 		}
 	},
 
+	prettifyDone = function() {
+
+		// Identify that initialization has completed
+		wb.ready( $document, componentName );
+	},
+
 	/*
 	 * Invoke the Google pretty print library if it has been initialized
 	 * @method prettyprint
@@ -115,10 +121,7 @@ var componentName = "wb-prettify",
 		if ( event.namespace === componentName &&
 			typeof window.prettyPrint === "function" ) {
 
-			window.prettyPrint();
-
-			// Identify that initialization has completed
-			wb.ready( $document, componentName );
+			window.prettyPrint( prettifyDone );
 		}
 	};
 
