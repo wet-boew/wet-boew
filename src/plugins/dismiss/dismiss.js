5c193d4d1b9b2a2d0324ea852efd90930d5e4f11
Etienne Levesque Guitard <etiennelg@gmail.com>
2014-08-14 15:40:12 -0400

    Totally WIP code, but backups

--- src/plugins/dismiss/dismiss.js
+++ src/plugins/dismiss/dismiss.js
@@ -4,9 +4,50 @@
  * @license wet-boew.github.io/wet-boew/License-en.html / wet-boew.github.io/wet-boew/Licence-fr.html
  * @author WET community
  */
- /*
 (function( $, window, document, wb ) {
 "use strict";
 
+if ( typeof(Storage) !== "undefined" ) {
+	// Code for localStorage/sessionStorage.
+
+	/*
+	 * Init State
+	 */
+	$("details.alert").each(function (index) {
+		if (index.attr("open")) {
+			if (index.attr("id")) {
+				localStorage.setItem("alert-dismiss-state-" + index.attr("id"), "open");
+			}
+		} else {
+			if (index.attr("id")) {
+				localStorage.setItem("alert-dismiss-state-" + index.attr("id"), "close");
+			}
+		}
+	});
+
+	/*
+	 * On page load events
+	 */
+	for( var key in localStorage ) {
+
+		$("details.alert").each(function (index) {
+
+		});
+
+		if ( localStorage.getItem(key) == "" )
+		localStorage.getItem(key);
+	}
+
+	/*
+	 * Event listeners for details elements
+	 */
+
+
+	// Retrieve
+	localStorage.getItem("lastname");
+
+} else {
+	// Sorry! No Web Storage support..
+}
+
 })( jQuery, window, document, wb );
-*/
