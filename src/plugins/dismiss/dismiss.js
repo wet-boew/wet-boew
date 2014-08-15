6c11c8c7acde2e525a96f6fb0394fe52407afe8a
Etienne Levesque Guitard <etiennelg@gmail.com>
2014-08-14 21:14:37 -0400

    Some more WIP

--- src/plugins/dismiss/dismiss.js
+++ src/plugins/dismiss/dismiss.js
@@ -4,50 +4,57 @@
  * @license wet-boew.github.io/wet-boew/License-en.html / wet-boew.github.io/wet-boew/Licence-fr.html
  * @author WET community
  */
-(function( $, window, document, wb ) {
+//(function( $, window, document, wb ) {
+(function( $ ) {
 "use strict";
 
 if ( typeof(Storage) !== "undefined" ) {
-	// Code for localStorage/sessionStorage.
 
 	/*
 	 * Init State
 	 */
-	$("details.alert").each(function (index) {
-		if (index.attr("open")) {
-			if (index.attr("id")) {
-				localStorage.setItem("alert-dismiss-state-" + index.attr("id"), "open");
+	$("details.alert").each(function(index) {
+
+		localStorage.setItem("Test", "test");
+		//alert(localStorage.getItem("Test"));
+		
+		console.log(this.attr("id"));
+		//alert(index.attr("id"));
+
+		/*
+		if (localStorage.getItem("alert-dismiss-state-" + this.attr("id"))) {
+
+			// Set open/closed state for existing localStorage keys
+			if (localStorage.setItem("alert-dismiss-state-" + this.attr("id")), "open") {
+				this.attr("open");
+			} else if (localStorage.setItem("alert-dismiss-state-" + this.attr("id")), "closed") {
+				this.removeAttr("open");
 			}
+
 		} else {
-			if (index.attr("id")) {
-				localStorage.setItem("alert-dismiss-state-" + index.attr("id"), "close");
+
+			// Set new localStorage values
+			if (this.attr("open")) {
+				if (this.attr("id")) {
+					localStorage.setItem("alert-dismiss-state-" + this.attr("id"), "open");
+				}
+			} else {
+				if (this.attr("id")) {
+					localStorage.setItem("alert-dismiss-state-" + this.attr("id"), "closed");
+				}
 			}
+
 		}
+		*/
+
 	});
-
-	/*
-	 * On page load events
-	 */
-	for( var key in localStorage ) {
-
-		$("details.alert").each(function (index) {
-
-		});
-
-		if ( localStorage.getItem(key) == "" )
-		localStorage.getItem(key);
-	}
 
 	/*
 	 * Event listeners for details elements
 	 */
 
-
-	// Retrieve
-	localStorage.getItem("lastname");
-
 } else {
 	// Sorry! No Web Storage support..
 }
 
-})( jQuery, window, document, wb );
+})( jQuery );
