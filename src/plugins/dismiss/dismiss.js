59fe4b6c4f0b30d0fa60d11eb85a085cc2dc7c10
Etienne Levesque Guitard <etiennelg@gmail.com>
2014-08-16 13:01:48 -0400

    Added dismissible alert plugin
    There's also a few modifications
    to the Sass to allow alert classes
    to be used with details/summary elements.

---
+++ src/plugins/dismiss/dismiss.js
@@ -0,0 +1,115 @@
+/**
+ * @title WET-BOEW Dismiss plugin
+ * @overview Dismiss alerts (details/summary)
+ * @license wet-boew.github.io/wet-boew/License-en.html / wet-boew.github.io/wet-boew/Licence-fr.html
+ * @author WET community
+ */
+(function( $, window, wb ) {
+"use strict";
+
+/*
+ * Variable and function definitions.
+ * These are global to the event - meaning that they will be initialized once per page,
+ * not once per instance of event on the page.
+ */
+var componentName = "wb-dismiss",
+	selector = "details.alert",
+	initEvent = "wb-init." + componentName,
+	$document = wb.doc,
+	details, key,
+
+	/**
+	 * @method init
+	 * @param {jQuery Event} event Event that triggered the function call
+	 */
+	init = function( event ) {
+
+		// Start initialization
+		// returns DOM object = proceed with init
+		// returns undefined = do not proceed with init (e.g., already initialized)
+		details = wb.init( event, componentName, selector );
+
+		if ( details ) {
+
+			key = "alert-dismiss-state-" + details.getAttribute("id");
+
+			try {
+				if ( localStorage.getItem( key ) ) {
+
+					// Set open/closed state for existing localStorage keys
+					if ( localStorage.getItem( key ) === "open" ) {
+						details.setAttribute( "open", "open" );
+						details.className += " open";
+					} else if ( localStorage.getItem( key ) === "closed" ) {
+						details.removeAttribute( "open" );
+						details.className = details.className.replace( " open", "" );
+					}
+
+				} else {
+
+					// Set new localStorage values
+					if ( details.hasAttribute( "open" ) ) {
+						localStorage.setItem( key, "open" );
+					} else {
+						localStorage.setItem( key, "closed" );
+					}
+
+				}
+			} catch ( e ) {}
+
+			// Identify that initialization has completed
+			wb.ready( $document, componentName );
+		}
+	};
+
+// Bind the init event of the plugin
+$document.on( "timerpoke.wb " + initEvent, selector, init );
+
+$document.on( "timerpoke.wb", function() {
+
+	// Do not bind events if details polyfill is active
+	if ( Modernizr.details ) {
+
+		// Bind the the event handlers of the plugin
+		$document.on( "click keydown toggle." + componentName, selector + " summary", function( event ) {
+			var which = event.which,
+				currentTarget = event.currentTarget,
+				isClosed;
+
+			// Ignore middle/right mouse buttons and wb-toggle enhanced summary elements (except for toggle)
+			if ( ( !which || which === 1 ) &&
+				( currentTarget.className.indexOf( "wb-toggle" ) === -1 ||
+				( event.type === "toggle" && event.namespace === componentName ) ) ) {
+
+				details = currentTarget.parentNode;
+				isClosed = details.getAttribute( "open" ) === null ;
+				key = "alert-dismiss-state-" + details.getAttribute( "id" );
+
+				if ( isClosed ) {
+					try {
+						localStorage.setItem( key, "open" );
+					} catch ( e ) {}
+				} else {
+					try {
+						localStorage.setItem( key, "closed" );
+					} catch ( e ) {}
+				}
+			} else if ( which === 13 || which === 32 ) {
+				event.preventDefault();
+				$( currentTarget ).trigger( "click" );
+			}
+
+			/*
+			 * Since we are working with events we want to ensure that we are being passive about our control,
+			 * so returning true allows for events to always continue
+			 */
+			return true;
+		});
+	}
+
+});
+
+// Add the timer poke to initialize the plugin
+wb.add( selector );
+
+})( jQuery, window, wb );
--- src/polyfills/details/details.js
+++ src/polyfills/details/details.js
@@ -51,7 +51,7 @@ $document.on( "timerpoke.wb " + initEvent, selector, init );
 $document.on( "click keydown toggle." + componentName, selector, function( event ) {
 	var which = event.which,
 		currentTarget = event.currentTarget,
-		details, isClosed;
+		details, isClosed, key;
 
 	// Ignore middle/right mouse buttons and wb-toggle enhanced summary elements (except for toggle)
 	if ( ( !which || which === 1 ) &&
@@ -61,12 +61,26 @@ $document.on( "click keydown toggle." + componentName, selector, function( event
 		details = currentTarget.parentNode;
 		isClosed = details.getAttribute( "open" ) === null ;
 
+		if ( details.className.indexOf( "alert" ) !== -1 ) {
+			key = "alert-dismiss-state-" + details.getAttribute( "id" );
+		}
+
 		if ( isClosed ) {
 			details.setAttribute( "open", "open" );
 			details.className += " open";
+			if ( key ) {
+				try {
+					localStorage.setItem( key, "open" );
+				} catch ( e ) {}
+			}
 		} else {
 			details.removeAttribute( "open" );
 			details.className = details.className.replace( " open", "" );
+			if ( key ) {
+				try {
+					localStorage.setItem( key, "closed" );
+				} catch ( e ) {}
+			}
 		}
 		currentTarget.setAttribute( "aria-expanded", isClosed );
 	} else if ( which === 13 || which === 32 ) {
