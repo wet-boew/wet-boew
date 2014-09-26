ff0d1e9aaf45769079380710be6fa85e4e494cf2
Paul Jackson <Paul.Jackson@tbs-sct.gc.ca>
2014-09-26 11:51:55 -0400

    Menu: Menu panel now sources the panel title from the page markup
    Fixes #6097

--- src/plugins/menu/menu.js
+++ src/plugins/menu/menu.js
@@ -20,7 +20,6 @@ var componentName = "wb-menu",
 	navCurrentEvent = "navcurr.wb",
 	focusEvent = "setfocus.wb",
 	menuItemSelector = "> a, > details > summary",
-	i18n, i18nText,
 	$document = wb.doc,
 
 	// Used for half second delay on showing/hiding menus because of mouse hover
@@ -48,14 +47,6 @@ var componentName = "wb-menu",
 				$elm.attr( "id", componentName + "-" + menuCount );
 			}
 			menuCount += 1;
-
-			// Only initialize the i18nText once
-			if ( !i18nText ) {
-				i18n = wb.i18n;
-				i18nText = {
-					searchMenus: i18n( "srch-menus" )
-				};
-			}
 
 			// Lets test to see if we have any menus to fetch
 			ajaxFetch = $elm.data( "ajax-fetch" );
@@ -309,8 +300,10 @@ var componentName = "wb-menu",
 
 		// Let's now populate the DOM since we have done all the work in a documentFragment
 		panelDOM.innerHTML = "<header class='modal-header'><div class='modal-title'>" +
-				i18nText.searchMenus + "</div></header><div class='modal-body'>" +
-				panel + "</div>";
+				document.getElementById( "wb-glb-mn" )
+					.getElementsByTagName( "h2" )[ 0 ]
+						.innerHTML +
+				"</div></header><div class='modal-body'>" + panel + "</div>";
 		panelDOM.className += " wb-overlay modal-content overlay-def wb-panel-r";
 		$panel
 			.trigger( "wb-init.wb-overlay" )
