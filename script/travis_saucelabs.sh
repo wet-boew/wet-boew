8c9bb1049d91c5e9e4a1921d176db1d9d5eff85c
Laurent Goderre <laurent.goderre@gmail.com>
2014-09-30 09:58:28 -0400

    Build: Move the saucelabs to grunt and optimized the .travis.yml

--- script/travis_saucelabs.sh
+++
@@ -1,9 +0,0 @@
-function error_exit
-{
-	echo -e "\e[01;31m$1\e[00m" 1>&2
-	exit 1
-}
-
-if [ "$TRAVIS_SECURE_ENV_VARS" == "true" ] && [ "$TRAVIS_REPO_SLUG" == "wet-boew/wet-boew" ]; then
-	grunt saucelabs || error_exit "Error running Sauce tests";
-fi
--- script/travis_script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -e
-
-scss-lint .
-grunt travis
