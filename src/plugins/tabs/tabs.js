wet-boew/wet-boew

commit e3363c348b3a1609c1e8b78b447f4787d836619c

[Tabbed Interface] - tighten the plugin selector to skip single item
- fixes https://github.com/wet-boew/wet-boew/issues/7428

src/plugins/tabs/tabs.js
@@ -14,7 +14,7 @@
  * variables that are common to all instances of the plugin on a page.
  */
 var componentName = "wb-tabs",
-	selector = "." + componentName,
+	selector = "." + componentName + ":has( > .tabpanels > [role='tabpanel']:nth-of-type(2), > .tabpanels > details:nth-of-type(2), > [role='tabpanel']:nth-of-type(2), > details:nth-of-type(2))",
 	initEvent = "wb-init" + selector,
 	shiftEvent = "wb-shift" + selector,
 	selectEvent = "wb-select" + selector,
@@ -71,15 +71,6 @@ var componentName = "wb-tabs",
 			$panels = $elm.find( "> .tabpanels > [role=tabpanel], > .tabpanels > details" );
 			$tablist = $elm.children( "[role=tablist]" );
 			isCarousel = $tablist.length !== 0;
-
-			// Stop carousel if only 1 panel
-			if ( isCarousel && $panels.length === 1 ) {
-
-				// Remove tabs roles
-				$tablist.remove();
-				$panels.removeAttr( "role" );
-				return true;
-			}
 
 			activeId = wb.jqEscape( wb.pageUrlParts.hash.substring( 1 ) );
 			hashFocus = activeId.length !== 0;

src/plugins/tabs/_base.scss
@@ -332,7 +332,11 @@ $tab-margin-width: 10px;
 	 */
 	&.carousel-s2 {
 		background: $carousel-s2-tablist-controls-bg-color;
-		padding-bottom: 4.375em;
+
+		// Only need to show controls when JS is active and plugin working
+		&.wb-init {
+			padding-bottom: 4.375em;
+		}
 
 		[role="tabpanel"] {
 			@extend %carousel-link;

src/plugins/tabs/tabs-carousel-en.hbs
@@ -301,6 +301,27 @@
 					</div>
 				</div>
 			</div>
+            <!--
+
+               -->
+            <h3>Carousel - Style 1 - Only one choice (slider controls and tranisitions are not applied)</h3>
+            <div class="wb-tabs carousel-s1">
+                <ul role="tablist">
+                    <li><a href="#panels7">Tab 1: Take Note: Renewal of the Aviation Document Booklet.</a></li>
+                </ul>
+                <div class="tabpanels">
+                    <div role="tabpanel" id="panels7" class="in fade">
+                        <a href="http://www.tc.gc.ca/eng/civilaviation/opssvs/general-personnel-changes-1814.htm">
+                            <figure>
+                                <img src="img/protect-environment.jpg" alt="" />
+                                <figcaption>
+                                    <p>Take Note: Renewal of the Aviation Document Booklet.</p>
+                                </figcaption>
+                            </figure>
+                        </a>
+                    </div>
+                </div>
+            </div>
 		</section>
 		<section class="col-sm-6 col-md-8">
 			<h3>Carousel style 2 - col-sm-6 col-md-8</h3>
@@ -777,4 +798,24 @@
 			</details>
 		</section>
 	</section>
+    <section>
+        <h3>Carousel - Style 2 - Only one choice (slider controls and tranisitions are not applied)</h3>
+        <div class="wb-tabs carousel-s2 show-thumbs playing slow">
+            <ul role="tablist">
+                <li><a href="#paneln2">Tab 2: Take Note: Grade Crossing Improvement Program (GCIP).</a></li>
+            </ul>
+            <div class="tabpanels">
+                <div role="tabpanel" id="paneln2" class="in fade">
+                    <a href="http://www.tc.gc.ca/eng/railsafety/publications-46.htm">
+                        <figure>
+                            <img src="img/banff.jpg" alt="" />
+                            <figcaption>
+                                <p>Take Note: Grade Crossing Improvement Program (GCIP).</p>
+                            </figcaption>
+                        </figure>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
 </section>

src/plugins/tabs/tabs-carousel-fr.hbs
@@ -177,6 +177,47 @@
 	</section>
 </section>
 
+<h2>Carrousel - Style 1 - Exemple (<code>carousel-s1</code>)</h2>
+<div class="wb-tabs carousel-s1">
+    <ul role="tablist">
+        <li class="active"><a href="#panels1">Onglet	1: Prenez note : Renouvellement du carnet de documents d'aviation.</a></li>
+        <li><a href="#panels2">Onglet 2: Prenez note : Programme d'amélioration des passages à niveau (PAPN).</a></li>
+        <li><a href="#panels3">Onglet 3: Prenez note : Tanker Groupe d'experts sur la sécurité.</a></li>
+    </ul>
+    <div class="tabpanels">
+        <div role="tabpanel" id="panels1" class="in fade">
+            <a href="http://www.tc.gc.ca/fra/aviationcivile/opssvs/generale-personnel-changements-1814.htm">
+                <figure>
+                    <img src="img/protect-environment.jpg" alt="" />
+                    <figcaption>
+                        <p>Prenez note : Renouvellement du carnet de documents d'aviation.</p>
+                    </figcaption>
+                </figure>
+            </a>
+        </div>
+        <div role="tabpanel" id="panels2" class="out fade">
+            <a href="http://www.tc.gc.ca/fra/securiteferroviaire/publications-46.htm">
+                <figure>
+                    <img src="img/banff.jpg" alt="" />
+                    <figcaption>
+                        <p>Prenez note : Programme d'amélioration des passages à niveau (PAPN).</p>
+                    </figcaption>
+                </figure>
+            </a>
+        </div>
+        <div role="tabpanel" id="panels3" class="out fade">
+            <a href="http://www.tc.gc.ca/fra/comiteexpertssecuritenaviresciternes/menu.htm">
+                <figure>
+                    <img src="img/investinourfuture.jpg" alt="" />
+                    <figcaption>
+                        <p>Prenez note : Tanker Groupe d'experts sur la sécurité.</p>
+                    </figcaption>
+                </figure>
+            </a>
+        </div>
+    </div>
+</div>
+
 <section>
 	<h2 id="carousel-s2-thmb">Carrousel - Style 2 avec imagette - Exemple (<code>carousel-s2 show-thumbs</code>)</h2>
 	<div class="wb-tabs carousel-s2 show-thumbs">
@@ -301,6 +342,25 @@
 					</div>
 				</div>
 			</div>
+
+            <h3>Carrousel - Style 1 - Une option (pas des légendes superposées et des controls)</h3>
+            <div class="wb-tabs carousel-s1">
+                <ul role="tablist">
+                    <li><a href="#panels7">Onglet 1: Prenez note : Renouvellement du carnet de documents d'aviation.</a></li>
+                </ul>
+                <div class="tabpanels">
+                    <div role="tabpanel" id="panels7" class="in fade">
+                        <a href="http://www.tc.gc.ca/fra/aviationcivile/opssvs/generale-personnel-changements-1814.htm">
+                            <figure>
+                                <img src="img/protect-environment.jpg" alt="" />
+                                <figcaption>
+                                    <p>Prenez note : Renouvellement du carnet de documents d'aviation.</p>
+                                </figcaption>
+                            </figure>
+                        </a>
+                    </div>
+                </div>
+            </div>
 		</section>
 		<section class="col-sm-6 col-md-8">
 			<h3>Carrousel - Style 2 - col-sm-6 col-md-8</h3>
@@ -765,4 +825,24 @@
 			</details>
 		</section>
 	</section>
+    <section>
+        <h2>Carrousel - Style 2 - Une option (pas des légendes superposées et des controls)</h2>
+        <div class="wb-tabs carousel-s2 show-thumbs">
+            <ul role="tablist">
+                <li class="active"><a href="#panels30"><img src="img/protect-environment.jpg" alt="Onglet 1: Prenez note : Renouvellement du carnet de documents d'aviation." /></a></li>
+            </ul>
+            <div class="tabpanels">
+                <div role="tabpanel" id="panels30" class="in fade">
+                    <a href="http://www.tc.gc.ca/fra/aviationcivile/opssvs/generale-personnel-changements-1814.htm">
+                        <figure>
+                            <img src="img/protect-environment.jpg" alt="" />
+                            <figcaption>
+                                <p>Prenez note : Renouvellement du carnet de documents d'aviation.</p>
+                            </figcaption>
+                        </figure>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
 </section>

commit 16898c08c89795ef7cf3730809ec8c6e72640bf1

Fixed exceptions in tabs caused by invalid event names
Introduced by e3363c348b3a1609c1e8b78b447f4787d836619c

src/plugins/tabs/tabs.js
@@ -14,11 +14,12 @@
  * variables that are common to all instances of the plugin on a page.
  */
 var componentName = "wb-tabs",
-	selector = "." + componentName + ":has( > .tabpanels > [role='tabpanel']:nth-of-type(2), > .tabpanels > details:nth-of-type(2), > [role='tabpanel']:nth-of-type(2), > details:nth-of-type(2))",
-	initEvent = "wb-init" + selector,
-	shiftEvent = "wb-shift" + selector,
-	selectEvent = "wb-select" + selector,
-	updatedEvent = "wb-updated" + selector,
+	namespace = "." + componentName,
+	selector = namespace + ":has( > .tabpanels > [role='tabpanel']:nth-of-type(2), > .tabpanels > details:nth-of-type(2), > [role='tabpanel']:nth-of-type(2), > details:nth-of-type(2))",
+	initEvent = "wb-init" + namespace,
+	shiftEvent = "wb-shift" + namespace,
+	selectEvent = "wb-select" + namespace,
+	updatedEvent = "wb-updated" + namespace,
 	setFocusEvent = "setfocus.wb",
 	controls = selector + " ul[role=tablist] a, " + selector + " ul[role=tablist] .tab-count",
 	initialized = false,