3595c5f7fdb112d8ccfa641bff051c902f09c8a4
Atam Singh <atamjeetsingh@cmail.carleton.ca>
2016-06-13 22:54:27 -0400

    teleHyperlink: adds hyperlinks to all phone numbers on mobile devices.
    The plugin locates any 10 digit phone number with and without
    hyphenation, brackets etc. and adds an a tel: link as href to the phone
    number.

---
+++ src/plugins/telephoneLink/tele.js
@@ -0,0 +1,21 @@
+/**
+ * @title WET-BOEW Telephone Link
+ * @overview Automatically add links to telephone numbers in a Web page on a extra small screen (mobile phones).
+ * @license wet-boew.github.io/wet-boew/License-en.html / wet-boew.github.io/wet-boew/Licence-fr.html
+ * @author @atamsingh
+ */
+
+function addTeleLink(){
+	if(screen.width < 650){
+		var regex = /(|\()(\d{3}).{0,2}(\d{3}).{0,1}(\d{4})(?!([^<]*>)|(((?!<a).)*<\/a>))/g; 
+	    var text = $("main").html();
+
+	    text = text.replace(regex, "<a href=\"tel:$&\">$&</a>");
+	    //console.log(text);
+	    $("main").html(text);
+	}
+}
+
+$(document).ready(function(){
+	addTeleLink();
+});
