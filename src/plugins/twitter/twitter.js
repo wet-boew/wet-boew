ad8588f912c017f36d8a48052a90ce7147b6a109
PierreCorriveau <58821067+PierreCorriveau@users.noreply.github.com>
2020-01-07 14:59:53 -0500

    Proposed solution for "Twitter feed not loading in IE"
    For those with AEM or another CMS that does not let you edit the code, they could simply update their wet-beow.js with that new Twitter.js version.
    
    Note: Since our current website does not have WET 4 and that we are not on Canada.ca, the language part starting at "var pathName" has not been tested but should be working. Please adapt the condition (if, else if) as needed to the URL that you have on your website.

--- src/plugins/twitter/twitter.js
+++ src/plugins/twitter/twitter.js
@@ -13,23 +13,21 @@
  * not once per instance of plugin on the page. So, this is a good place to define
  * variables that are common to all instances of the plugin on a page.
  */
+
 var componentName = "wb-twitter",
 	selector = "." + componentName,
 	initEvent = "wb-init" + selector,
 	$document = wb.doc,
-
 	/**
 	 * @method init
 	 * @param {jQuery Event} event Event that triggered the function call
 	 */
 	init = function( event ) {
-
 		// Start initialization
 		// returns DOM object = proceed with init
 		// returns undefined = do not proceed with init (e.g., already initialized)
 		var eventTarget = wb.init( event, componentName, selector ),
 			protocol = wb.pageUrlParts.protocol;
-
 		if ( eventTarget ) {
 			Modernizr.load( {
 				load: ( protocol.indexOf( "http" ) === -1 ? "http:" : protocol ) + "//platform.twitter.com/widgets.js",
@@ -38,13 +36,28 @@ var componentName = "wb-twitter",
 					// Identify that initialization has completed
 					wb.ready( $( eventTarget ), componentName );
 				}
-			} );
+			} );		
 		}
 	};
 
-$document.on( "timerpoke.wb " + initEvent, selector, init );
-
+var ua = navigator.userAgent;
+/* If the user's browser is IE, don't load the Twitter widget and display a message */
+if (ua.indexOf("MSIE ") > -1 || ua.indexOf("Trident/") > -1) {
+   $( '.wb-twitter a' ).removeClass( 'twitter-timeline' );
+   $( '.wb-twitter' ).after( '<p id="ieNotSupportedMessage">' );
+   var pathName = window.location.pathname; 
+   var pageLanguage = str.substring(1, 3);
+   if (pageLanguage === "en") {
+      $('#ieNotSupportedMessage').text("Your browser does not support the display of the Twitter feed. Please try another browser.");
+   }
+   else if (pageLanguage === "fr") {
+      $('#ieNotSupportedMessage').text("Votre navigateur ne supporte pas l'affichage du fil Twitter. Veuillez essayer un autre navigateur.");
+   }
+}
+/* Any other browsers, load the Twitter widget */
+else {
+   $document.on( "timerpoke.wb " + initEvent, selector, init );
+}
 // Add the timer poke to initialize the plugin
 wb.add( selector );
-
 } )( jQuery, window, wb );
