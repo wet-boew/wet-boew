ae3187ff86fcc5a25c97c80783d7fd2e5c824735
Eric Dunsworth <1907279+EricDunsworth@users.noreply.github.com>
2020-09-15 17:23:24 -0400

    Forms: Fix YYYY-MM-DD/AAAA-MM-JJ date format labels
    * Change a mention of "YYYY-MM-DD" to "AAAA-MM-JJ" in the date picker polyfill's French documentation
    * Add abbreviation into one of the date picker polyfill's unit tests
    * Add abbreviation into the form validation demo's date field
    * Add date format into the steps form demo's date of birth label
    * Partially addresses #8916

--- src/polyfills/datepicker/test.js
+++ src/polyfills/datepicker/test.js
@@ -114,7 +114,7 @@ runTest( "Input type=\"date\" polyfill (date picker)", function() {
 	describe( "with a date format and error in the label", function() {
 		var label = "<label for=\"appointment\">" +
 				"<span class=\"field-name\">Appointment Date</span>" +
-				"<span class=\"datepicker-format\">(YYYY-MM-DD)</span>" +
+	"<span class=\"datepicker-format\"> (<abbr title=\"Four digits year, dash, two digits month, dash, two digits day\">YYYY-MM-DD</abbr>)</span>" +
 				"<strong class=\"error\" id=\"appointment-error\">" +
 					"<span class=\"label label-danger\">" +
 						"<span class=\"prefix\">Error 1: </span>Please enter a valid date" +
