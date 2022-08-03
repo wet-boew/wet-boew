1aeb0f5d12f7e55a8f2a5fb78003a4e1076788f6
Eric Dunsworth <1907279+EricDunsworth@users.noreply.github.com>
2022-08-03 13:17:17 -0400

    Site: Update more HTTP links to HTTPS
    * Update links to HTTP sites that now support HTTPS and/or redirect to new locations on HTTPS sites
    * Change links to HTTP sites that no longer exist to point towards Wayback Machine mirrors
    * Spinoff of #9395's review comment
    * Successor to #8674

--- src/polyfills/slider/slider.js
+++ src/polyfills/slider/slider.js
@@ -25,7 +25,7 @@ var fdSlider = (function() {
             // Does a JSON (native or not) Object exist
             if(typeof JSON === "object" && typeof(JSON.parse) === "function") {
                 return JSON.parse(str);
-            // Genious code taken from: http://kentbrewster.com/badges/
+            // Genious code taken from: https://web.archive.org/web/20140715200301/http://kentbrewster.com/badges/
             } else if(/mousewheelenabled|fullaria|describedby|norangebar|html5animation|varsetrules/.test(str.toLowerCase())) {
                 var f = Function(['var document,top,self,window,parent,Number,Date,Object,Function,',
                     'Array,String,Math,RegExp,Image,ActiveXObject;',
