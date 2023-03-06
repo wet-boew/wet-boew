fcfa30364a772f7891844c0fc48f2c3de0403c2f
Eric Dunsworth <1907279+EricDunsworth@users.noreply.github.com>
2023-03-06 14:23:47 -0500

    Postback: Fix missing submit button variable
    The postback plugin was previously excluding submit button variables in the following scenarios:
    1. When the submit button was a type-less button element (the button element's default type is submit according to the HTML spec)
    2. When the form validation plugin was used
    
    This fixes it by:
    1. Adjusting button selectors as follows:
       * Expand the $buttons selector to cover type-less button elements
       * Remove [type=submit] from the $btn selector (what remains should be able to pick up any engaged submit button associated to a variable)
    2. Removing the data-wb-engaged attribute from a clicked submit button if form validation fails (opposite of what was previously being done)
    
    Also updates the demo page's examples to better represent the fixed scenarios:
    * Add submit button variable examples (name/value attributes):
      * Standard form with just a submit button
      * Form validation plugin with one required field and a submit button
    * Remove all submit buttons' type="submit" attributes
    * Gruntfile: Add exclusion for bootlint's W007 rule to suppress errors on type-less button elements
    * Place all examples into their own sections

--- src/plugins/wb-postback/wb-postback.js
+++ src/plugins/wb-postback/wb-postback.js
@@ -27,7 +27,7 @@ var $document = wb.doc,
 					wb.getData( $elm, componentName )
 				),
 				attrEngaged = "data-wb-engaged",
-				$buttons = $( "[type=submit]", $elm ),
+				$buttons = $( "[type=submit], button:not([type])", $elm ),
 				multiple = typeof $elm.data( componentName + "-multiple" ) !== "undefined",
 				classToggle = settings.toggle || "hide",
 				selectorSuccess = settings.success,
@@ -48,15 +48,15 @@ var $document = wb.doc,
 				if ( elm.parentElement.classList.contains( "wb-frmvld" ) ) {
 					if ( !$elm.valid() ) {
 						$( this ).attr( attrEngaged, true );
-					} else {
 						$buttons.removeAttr( attrEngaged );
+					} else {
 						$( this ).attr( attrEngaged, "" );
 					}
 				}
 
 				if ( !$( this ).attr( attrEngaged ) ) {
 					var data = $elm.serializeArray(),
-						$btn = $( "[type=submit][name][" + attrEngaged + "]", $elm ),
+						$btn = $( "[name][" + attrEngaged + "]", $elm ),
 						$selectorSuccess = $( selectorSuccess ),
 						$selectorFailure = $( selectorFailure );
 
