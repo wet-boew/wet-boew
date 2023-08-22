ce1712ee840fe6de9890567fe89ee81ffbc0a57a
Garneauma <garneauma@gmail.com>
2023-08-22 11:51:29 -0400

    Patch - Data-json: fixing issue with null value (#9651)
    * data-json: fixing issue with null value
    * Content - Add edge test case for data-json and json-manager
    
    Patch - Data-json: Fixing issue with null value
    
    ---------
    
    Co-authored-by: Pierre Dubois <duboisp2@gmail.com>

--- src/plugins/wb-data-json/data-json.js
+++ src/plugins/wb-data-json/data-json.js
@@ -289,7 +289,7 @@ var componentName = "wb-data-json",
 				content = [ content ];
 			} else {
 				content = $.map( content, function( val, index ) {
-					if ( typeof val === "object" && !$.isArray( val ) ) {
+					if ( val && typeof val === "object" && !$.isArray( val ) ) {
 						if ( !val[ "@id" ] ) {
 							val[ "@id" ] = index;
 						}
@@ -853,7 +853,7 @@ var componentName = "wb-data-json",
 		var value = getRawValue( source, pointer );
 
 		// for JSON-LD @value support
-		if ( typeof value === "object" && value !== null && value[ "@value" ] ) {
+		if ( typeof value === "object" && value !== null && Object.prototype.hasOwnProperty.call( value, "@value" ) ) {
 			value = value[ "@value" ];
 		}
 
