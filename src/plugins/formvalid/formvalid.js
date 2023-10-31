38141d0dab1267d3c05cdb1c2d04a3ea143efd44
Stewart Downey <stewart.downey@hrsdc-rhdcc.gc.ca>
2023-10-31 11:16:44 -0500

    Formvalid: Update server message page examples.

--- src/plugins/formvalid/formvalid.js
+++ src/plugins/formvalid/formvalid.js
@@ -385,9 +385,9 @@ var componentName = "wb-frmvld",
 							if ( $( this ).find( $( this ).next( ".radio-inline" ).children( "input " )[ 0 ].id ) ) {
 								$form.validate().element( $( this ).next( ".radio-inline" ).children( "input" )[ 0 ] );
 							}
-						} else if ( $( this ).next( ".form-inline" ).children( "li" ).children( "input" )[ 0 ] ) {
-							if ( $( this ).find( $( this ).next( ".form-inline" ).children( "li" ).children( "input " )[ 0 ].id ) ) {
-								$form.validate().element( $( this ).next( ".form-inline" ).children( "li" ).children( "input" )[ 0 ] );
+						} else if ( $( this ).next( ".label-inline" ).children( "input" )[ 0 ] ) {
+							if ( $( this ).find( $( this ).next( ".label-inline" ).children( "input " )[ 0 ].id ) ) {
+								$form.validate().element( $( this ).next( ".label-inline" ).children( "input" )[ 0 ] );
 							}
 						}
 					} );
