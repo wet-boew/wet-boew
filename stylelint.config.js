8db6c5b72841ab4d3ed832644bce2abc4863d748
Nick Schonning <nschonni@gmail.com>
2025-03-13 19:38:59 -0400

    build: add stylelint-order plugin

--- stylelint.config.js
+++ stylelint.config.js
@@ -101,7 +101,15 @@ module.exports = {
 			extends: [
 				"stylelint-config-standard-scss"
 			],
+			plugins: [
+				"stylelint-order"
+			],
 			rules: {
+
+				// Additional rules for replacing sass-lint
+				"order/properties-alphabetical-order": true,
+
+				// Suppress stylelint-config-standard errors
 				"declaration-block-no-redundant-longhand-properties": null,
 				"no-descending-specificity": null, // Extremely slow
 				"no-invalid-position-at-import-rule": null, // Fixable, need to run sass migrator
