wet-boew/wet-boew

commit aa8b1880dfda17ef3ff14111ba264102d8fbf0cb

Patch - Overlay: Fix reverse-tabbing after opening overlays (#10153)
It was previously possible to close most overlays by reverse-tabbing after opening them - with the following exceptions:
* Focusing forward beforehand bypassed the issue
* Lightbox overlays were unaffected

The buggy behaviour was caused by the Tab key press logic's index variable being based on whether event.target existed in the $focusable array - which isn't the case when an overlay is first opened (event.target is initially the overlay container itself).

Since the index variable is based on adding the event target's $focusable array index (-1 in this case since the container doesn't exist in the array) and a number derived from pressing the Shift key (-1)... the index variable ended up being -2.

The plugin logic that detects whether it needs to force focus to the beginning or end of the overlay container was only designed to handle scenarios where index is -1 or matches the array's length. So it became possible to unexpectedly "escape" the overlay when index was -2.

This resolves it by adjusting the buggy logic to check for < 0 instead of === -1.

src/plugins/overlay/overlay.js
@@ -211,9 +211,9 @@ $document.on( "timerpoke.wb " + initEvent + " keydown open" + selector +
 						length = $focusable.length;
 						index = $focusable.index( event.target ) + ( event.shiftKey ? -1 : 1 );
 
-						if ( index === -1 || index === length ) {
+						if ( index < 0 || index === length ) {
 							event.preventDefault();
-							$focusable.eq( index === -1 ? length - 1 : 0 )
+							$focusable.eq( index < 0 ? length - 1 : 0 )
 								.trigger( setFocusEvent );
 						}
 					}

commit 5f1bdefbdb450e482a74c956d43fceac3e20792c

Patch - Overlay: Fix reverse-tabbing from tabindex=-1 elements (#10161)
Prior to #10153, if an overlay was open and focus was set on any element with a tabindex="-1" attribute, reverse-focusing (Tab+Shift) behaved normally (went to the previous tabbable element).

However, a regression in #10153 caused the plugin to misjudge that scenario as an attempt to reverse-tab to the overlay's last tabbable element - resulting in the plugin forcing focus onto the X button.

Specifically, it was caused by the Tab key press logic's index variable getting set to -2 when reverse-tabbing from any element with tabindex="-1". Why? Because those elements don't exist in the $focusable array (variable name is misleading - it only contains tabbable elements...). So when the plugin tried finding the currently-focused element's index in $focusable, no hits were found and -1 was returned. Then it got added to another number (-1) derived from pressing the Shift key. End result was that the plugin acted as if the initially-focused element was the overlay container itself.

This resolves it by revising the aforementioned logic to:
* Check whether event.target is the overlay container (new behaviour) OR if index is exactly -1 (pre-#10153 behaviour)
* Focus onto the last tabbable element if index is < 0 (#10153's behaviour - won't reach this point unless the first check passes)

Note:
To test, try reverse-tabbing while focused onto any link in the mobile menu's expanded dropdowns. Focus should return to a summary element (as opposed to the X button).

src/plugins/overlay/overlay.js
@@ -211,7 +211,7 @@ $document.on( "timerpoke.wb " + initEvent + " keydown open" + selector +
 						length = $focusable.length;
 						index = $focusable.index( event.target ) + ( event.shiftKey ? -1 : 1 );
 
-						if ( index < 0 || index === length ) {
+						if ( event.target === overlay || index === -1 || index === length ) {
 							event.preventDefault();
 							$focusable.eq( index < 0 ? length - 1 : 0 )
 								.trigger( setFocusEvent );